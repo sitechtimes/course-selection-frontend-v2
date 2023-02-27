1a2b578fbfbe29e2b550cbee1997c75857bfc23d
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-02-27 12:46:24 -0500

    done, but could use some work

--- src/views/landing.vue
+++ src/views/landing.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="h-screen w-full flex flex-row justify-center items-center">
-    <div id="left" class="w-11/12 flex flex-col justify-center items-center text-center lg:items-start lg:text-left lg:w-4/5 space-y-6">
+    <div id="left" class="w-11/12 flex flex-col justify-center items-center text-center space-y-4 lg:items-start lg:text-left lg:w-3/4 max-w-3xl lg:space-y-6 lg:ml-12">
       <h1 id="heading" class="text-4xl font-bold lg:text-5xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, consequuntur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 
       </h1>
       <h2 class="text-lg lg:text-xl">
@@ -24,6 +24,19 @@
         </button>
       </RouterLink>
       </div>
+    </div>
+    <div
+      id="circles"
+      class="-z-10 absolute h-96 w-112 sm:h-128 sm:w-128 lg:h-96 lg:w-112 flex justify-center items-center lg:relative lg:mx-8"
+    >
+      <div
+        id="big"
+        class="h-80 w-80 sm:h-96 sm:w-96 lg:h-80 lg:w-80 bg-tertiary-s rounded-full right-32"
+      ></div>
+      <div
+        id="small"
+        class="h-32 w-32 sm:h-40 sm:w-40 lg:h-32 lg:w-32 absolute bottom-4 left-12 bg-other-s rounded-full"
+      ></div>
     </div>
   </div>
 </template>
