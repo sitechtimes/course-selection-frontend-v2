b39be89a5509504c3306d01905734e28cdf7d29f
wrrius <2.dan.0.bay.0@gmail.com>
2023-02-27 12:29:57 -0500

    .

---
+++ src/views/SurveyPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- Page 1 (for everyone) -->
+    <h1>______ Year Survey</h1>     <!-- Fill in ______ with the year -->
+    <label for="major">What college majors are you considering upon graduation from high school?</label>
+    <input type="text" id="major"/>
+    <label for="career">What careers are you considering upon graduation?</label>
+    <input type="text" id="career"/>
+    <label for="afterSchool">List after-school activities such as clubs, teams, volunteer, or paid employment.</label>
+    <input type="text" id="afterSchool"/>
+</template>
+
+<style >
+    body {
+        margin-top: 5rem;
+    },
+    input {
+        border: 1rem solid solid;
+    }
+</style>
