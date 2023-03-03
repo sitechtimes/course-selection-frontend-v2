009b5b1b02a6f5daa6e26a61bce53b3250713d7a
nullZheng <zhenghenry2@gmail.com>
2023-03-03 13:45:20 -0500

    adding some paths to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../views/HomePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'landing',
-      component: () => import('../views/landing.vue')
+      name: 'home',
+      component: HomePage
+    },
+    // {
+    //   path: '/courses',
+    //   name: 'course',
+    //   component: () => import('../views/CourseCatalog.vue')
+    // },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginPage.vue')
+    },
+    {
+      path: '/survey',
+      name: 'survey',
+      component: () => import('../views/SurveyPage.vue')
     },
   ]
 })
