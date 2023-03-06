d1aa3bc63f699f01654f57d232a47538f2616e22
wrrius <2.dan.0.bay.0@gmail.com>
2023-03-06 12:30:10 -0500

    media querries

--- src/components/SurveyPageComponents/nextButton.vue
+++ src/components/SurveyPageComponents/nextButton.vue
@@ -1,8 +1,5 @@
 <template>
-  <button class="bg-[#6A9FD1] text-white w-[10rem] h-[3.5rem] text-[2rem] absolute mt-16 right-32">
+  <button class="bg-[#6A9FD1] text-white w-[7rem] h-[2rem] text-[1.5rem] md:w-[10rem] md:h-[3.5rem] md:text-[2rem] mt-8 md:absolute md:mt-16 md:right-32">
     Next
   </button>
 </template>
-
-<style scoped>
-</style>
--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="mt-[7rem] text-center"> <!-- This one is the same for every year -->
-    <h1 class="text-[#37394F] text-6xl">______ Year Survey</h1>     <!-- Fill in ______ with the year -->
-    <div class="mt-[3rem] text-4xl"><label for="major">What college majors are you considering upon graduation from high school?</label>
-    <input type="text" id="major" class="mt-[2rem] h-[4rem] w-[90rem] rounded-xl border-4 border-[#7D7D7D]"/></div>
-    <div class="mt-[3rem] text-4xl"><label for="career">What careers are you considering upon graduation?</label>
-    <input type="text" id="career" class="mt-[2rem] h-[4rem] w-[90rem] rounded-xl border-4 border-[#7D7D7D]"/></div>
-    <div class="mt-[3rem] text-4xl"><label for="afterSchool">List after-school activities such as clubs, teams, volunteer, or paid employment.</label>
-    <input type="text" id="afterSchool" class="mt-[2rem] h-[4rem] w-[90rem] rounded-xl border-4 border-[#7D7D7D]"/></div>
-    </section>
+    <h1 class="text-[#37394F] text-4xl md:text-6xl font-bold">______ Year Survey</h1>     <!-- Fill in ______ with the year -->
+    <div class="mt-[3rem] text-2xl md:text-4xl"><label for="major">What college majors are you considering upon graduation from high school?</label>
+    <input type="text" id="major" class="mt-[2rem] h-[4rem] w-[90%] lg:w-[80%] rounded-xl border-4 border-[#7D7D7D]"/></div>
+    <div class="mt-[3rem] text-2xl md:text-4xl"><label for="career">What careers are you considering upon graduation?</label>
+    <input type="text" id="career" class="mt-[2rem] h-[4rem] w-[90%] lg:w-[80%] rounded-xl border-4 border-[#7D7D7D]"/></div>
+    <div class="mt-[3rem] text-2xl md:text-4xl"><label for="afterSchool">List after-school activities such as clubs, teams, volunteer, or paid employment.</label>
+    <input type="text" id="afterSchool" class="mt-[2rem] h-[4rem] w-[90%] lg:w-[80%] rounded-xl border-4 border-[#7D7D7D]"/></div>
     <nextButton></nextButton>
+    </section>
 </template>
 
 <script setup lang="ts">
