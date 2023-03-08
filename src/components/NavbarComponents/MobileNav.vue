811d9ae493c17e636f9bc20a5c0d0e892be3e080
Henry Zheng <zhenghenry2@gmail.com>
2023-03-08 14:09:52 -0500

    changing z-index on mobile menu

--- src/components/NavbarComponents/MobileNav.vue
+++ src/components/NavbarComponents/MobileNav.vue
@@ -47,7 +47,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div id="menu" class="fixed inset-0 h-screen w-full  ease-in duration-200 md:hidden">
+    <div id="menu" class="fixed inset-0 h-screen w-full  ease-in duration-200 md:hidden z-30">
         <div id="overlay" class="absolute inset-0 w-full h-full bg-white opacity-100 -z-10"></div>
         <div v-if="userStore.isLoggedIn" class="h-full absolute top-48 left-16 flex flex-col justify-start items-start space-y-8 z-10">
             <RouterLink @click="$emit('e')" id="link" to="/survey"><p class="text-4xl">Survey</p></RouterLink>
