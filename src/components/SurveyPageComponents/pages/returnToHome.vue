0275ac972193e0f832f8277ae9eaf2635c94bf42
wrrius <2.dan.0.bay.0@gmail.com>
2023-03-09 12:34:46 -0500

    that guidance councelor end thing is doen

---
+++ src/components/SurveyPageComponents/pages/returnToHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="mt-[7rem] pb-9 text-center flex justify-center" > <!-- This one is the same for every year -->
+        <div class="border-white border-solid border-[1rem] w-[70%] rounded-lg ring-[#D9E9F9] ring-[2rem] mt-[7rem]"  >
+            <h1 class="text-[#37394F] text-4xl mt-32 md:text-6xl font-bold">Thank you for submitting!</h1>
+            <h2 class="text-3xl mt-24">Your guidance counselor will be reaching out to you.</h2>
+            <button class="bg-[#6A9FD1] text-white w-[30%] mt-40 h-[3.5rem] text-[1.5rem] md:w-[22rem] md:text-[2rem] mb-[2rem]">
+                Return to Home
+            </button>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped> 
+
+</style>
