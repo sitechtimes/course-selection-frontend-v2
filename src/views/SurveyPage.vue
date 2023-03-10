48d7fa9105c7e5d5310ca52af40f55e11c3759d8
wrrius <2.dan.0.bay.0@gmail.com>
2023-03-10 12:30:20 -0500

    That return to home page now has media queries.  Work on survey page confirmation

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,6 +1,14 @@
 <template>
-    <section class="" > <!-- This one is the same for every year -->
-
+    <section class="text-center flex justify-center mt-72 " >
+        <div class="border-white border-solid border-[1rem] w-[50%] rounded-lg ring-[#A3C2DF] ring-[0.5rem]">
+            <h1 class="text-[#37394F] font-bold text-5xl mb-16">By pressing Confirm, you are confirming that this is your order of preference for the course(s) you selected.</h1>
+            <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2.5rem] mr-8">
+                Edit
+            </button>
+            <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2.5rem] ml-8">
+                Confirm
+            </button>
+        </div>
     </section>
 </template>
 
