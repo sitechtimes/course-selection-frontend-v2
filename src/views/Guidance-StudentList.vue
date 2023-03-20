a8367b125033c5b0a9c392d3853b35581ae590f1
AgnethaDMello <agnethad@nycstudents.net>
2023-03-20 12:29:56 -0400

    ok!

--- src/views/Guidance-StudentList.vue
+++ src/views/Guidance-StudentList.vue
@@ -4,7 +4,7 @@ import DownArrow from '../components/icons/DownArrow.vue';
 </script>
 
 <template>
-    <div class="h-screen w-full flex justify-center items-center">
+    <div class="h-screen w-full flex flex-col justify-center items-center">
         <div class="ml-20 flex flex-row items-center">
         <button class="w-44 h-10 mr-4 bg-primary-g text-black rounded cursor-pointer shadow-[4px_3px_3px_rgba(0,0,0,0.25)]">
             <div class="font-semibold ml-4 flex justify-evenly">
@@ -12,9 +12,9 @@ import DownArrow from '../components/icons/DownArrow.vue';
                 <DownArrow/>
             </div>
         </button>
+        <SearchBar class="" type="text" placeholder="Search Students..." />
         </div>
-        <SearchBar type="text" placeholder="Search Students..." />
-    <div class="w-full flex flex-col justify-center items-center" id="table">
+    <div class="w-full flex flex-col mt-10 justify-center items-center" id="table">
         <table>
             <thead>
                 <tr>
