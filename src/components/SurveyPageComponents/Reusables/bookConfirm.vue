d6701ce24618ecb4b14364f301b2091155f405fd
wrrius <2.dan.0.bay.0@gmail.com>
2023-03-22 12:29:46 -0400

    dont remember

--- src/components/SurveyPageComponents/Reusables/bookConfirm.vue
+++ src/components/SurveyPageComponents/Reusables/bookConfirm.vue
@@ -1,19 +1,19 @@
 <template>
     <section class="mt-[2rem] md:mt-[5rem] pb-9 text-center flex justify-center " > <!-- This one is the same for every year -->
-        <div class="border-white border-solid border-[1rem] w-[65%] rounded-lg ring-[#A3C2DF] ring-[1rem] mt-[7rem] "  >
-            <img src="../../icons/Bookmark.png" class="w-16 mt-[-1rem] ml-6 absolute">
+        <div class="border-white border-solid border-[1rem] w-[65%] rounded-lg ring-[#A3C2DF] ring-[1rem] mt-[7rem]" >
+            <img src="../../icons/Bookmark.png" alt="" class="w-16 mt-[-1rem] ml-6 absolute">
             <h1 class="text-[#37394F] text-4xl mt-8 md:text-6xl font-bold">______ Year Survey</h1>     <!-- Fill in ______ with the year -->
             <div class="flex place-content-between h-[28rem] mt-6">
             <section class="w-[45%] h-[100%] flex flex-wrap items-center justify-center place-content-around">
                 <h2 class="font-bold text-3xl text-black">Rank Interested P.E. Courses</h2>
                 <div class="border-dashed border-[#7D7D7D] border-2 w-[22rem] h-[4rem] flex items-center rounded-2xl">
-                    <p class="text-[#606060] text-[2rem] ml-6">1.</p>
+                    <h2 class="text-[#606060] text-[2rem] ml-6">1.</h2>
                 </div> <!-- need a v-for in this part. -->
                 <div class="border-dashed border-[#7D7D7D] border-2 w-[22rem] h-[4rem] flex items-center rounded-2xl">
-                    <p class="text-[#606060] text-[2rem] ml-6">2.</p>
+                    <h2 class="text-[#606060] text-[2rem] ml-6">2.</h2>
                 </div>
                 <div class="border-dashed border-[#7D7D7D] border-2 w-[22rem] h-[4rem] flex items-center rounded-2xl">
-                    <p class="text-[#606060] text-[2rem] ml-6">3.</p>
+                    <h2 class="text-[#606060] text-[2rem] ml-6">3.</h2>
                 </div>
             </section>
             <div class="border-solid border-black border-[1px] h-[103%]"></div>
--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,12 +1,28 @@
 <template>
-    
+  <div class="mt-96 height-48">
+    <SurveyButton :form="form" :name="question" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import pageOne from '../components/SurveyPageComponents/pages/returnToHome.vue';
-import backButton from '../components/SurveyPageComponents/reusables/backButton.vue'
+import pageOne from "../components/SurveyPageComponents/pages/returnToHome.vue";
+import SurveyButton from "../components/SurveyPageComponents/reusables/surveyButton.vue";
+
+const form = [
+  {
+    id: "first-question",
+    answer: "Yes",
+  },
+  {
+    id: "second-question",
+    answer: "No",
+  },
+  {
+    id: "third-question",
+    answer: "Not Sure",
+  },
+];
+const question = "Are you interested in a 8th academic period (1-9 schedule)?";
 </script>
 
-<style scoped> 
-
-</style>
+<style scoped></style>
