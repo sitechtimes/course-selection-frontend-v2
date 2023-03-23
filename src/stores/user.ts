8071cc0b6428679a0404cddc0f14e4cfba62e2c3
Hashir <hashirabbasi112@gmail.com>
2023-03-23 10:10:34 -0400

    store some student data

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,37 +1,16 @@
 import { defineStore } from "pinia";
 import axios from 'axios'
+import {user, account_type, userData} from "../types/interface";
 
-interface user {
-    first_name: string
-    last_name: string
-    email: string
-    data: userData
-    grade?: grade
-    userType: account_type
-    isLoggedIn: boolean
-    access_token: string
-    refresh_token: string
-}
 
-interface userData { 
-    survey?: object //should be empty for guidance, filled for students
-    students?: studentData[] // should be empty for students, filled for guidance
-}
 
-interface studentData { //this is for guidance use 
-    first_name: string
-    last_name: string
-    survey: object, 
-    counselor: string,
-}
 
-type account_type = 'student' | 'guidance' | null;
-type grade = 'freshman' | 'sophomore' | 'junior' | 'senior';
+
 
 export const useUserStore = defineStore('user', {
     state: (): user => ({
         first_name: '',
-        data: {} ,
+        data: {},
         last_name: '',
         email: '',
         userType: null,
@@ -40,33 +19,53 @@ export const useUserStore = defineStore('user', {
         refresh_token: '',
     }),
     getters: {
-        getStudents(name?: string): object[] | object | undefined {
-            if (name) {
-                const students = this.data.students?.filter((e) => e.first_name === name);
-                return students
-            }
-            return this.data.students
-        },
+        // getStudents(name?: string): object[] | object | undefined {
+        //     if (name) {
+        //         const students = this.data.students?.filter((e) => e.first_name === name);
+        //         return students
+        //     }
+        //     return this.data.students
+        // },
     },
     actions: {
         async init(type: account_type) {
             this.userType = type;
             if (type === 'guidance') {
                 // get all data for counselor and set it here
-                const res = await fetch('api.siths.dev');
-                this.data.students = await res.json();
+                // const res = await fetch('api.siths.dev');
+                // this.data.students = await res.json();
             }
             else {
                 await axios.post('http://127.0.0.1:8000/graphql/', {
                     query:`query{
+                        user{
+                            firstName
+                            lastName
+                            email
+                            isActive
+                            isStudent
+                            isGuidance
+                        }
                         student{
-                            answeredsurveySet{
-                                edges{
-                                    node{
-                                        answers
-                                    }
-                                }
+                            homeroom
+                            coursesTaken{
+                                courseCode
                             }
+                            coursesRequired{
+                                courseCode
+                            }
+                            coursesAvailable{
+                                courseCode
+                            }
+                        }
+                        survey{
+                            questions{
+                                question
+                                questionType
+                            }
+                        }
+                        answeredSurvey{
+                            answers
                         }
                         }`,
                     },{
@@ -76,7 +75,9 @@ export const useUserStore = defineStore('user', {
                     }
     
                 }).then((res:any)=>{
-                    console.log(res.data) //properly handle survey reponse
+                    this.data = res.data.data // data needs to be filtered properly
+                    console.log(this.data)
+                    
                 })
             }
         },
@@ -84,11 +85,13 @@ export const useUserStore = defineStore('user', {
             await axios.post('http://127.0.0.1:8000/social-login/google/',{"access_token":res.access_token}
                 ).then((response)=>{
                     //clean this up :) 
+                    console.log(response.data.access_token)
                     this.access_token = response.data.access_token
                     this.refresh_token = response.data.refresh_token
                     this.email = response.data.user.email
                     this.first_name = response.data.user.first_name
                     this.last_name = response.data.user.last_name
+
                     console.log(this.first_name)
                     this.init('student') //make dj rest auth return user type (backend)
 
