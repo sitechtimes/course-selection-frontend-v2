a0d103774271346b0bb153f370d0f0bf419af086
wrrius <2.dan.0.bay.0@gmail.com>
2023-03-23 12:30:20 -0400

    survey page css

--- src/components/SurveyPageComponents/Reusables/surveyButton.vue
+++ src/components/SurveyPageComponents/Reusables/surveyButton.vue
@@ -4,7 +4,7 @@
     <div v-for="choice in form" :key="choice" class="circle-button">
       <input
         type="radio"
-        class="w-6 h-6 m-2 shadow-2xl outline-1 checked:border-1 checked:border-black outline-black checked:outline-black checked:outline-1 checked:outline-offset-0 bg-[#EAEAEA] focus:ring-transparent checked:bg-none checked:bg-[#6A9FD1] checked:hover:bg-[#6A9FD1] checked:focus:bg-[#6A9FD1]"
+        class="w-6 h-6 m-2 outline-1 checked:border-1 checked:border-black outline-black checked:outline-black checked:outline-1 checked:outline-offset-0 bg-[#EAEAEA] focus:ring-transparent checked:bg-none checked:bg-[#6A9FD1] checked:hover:bg-[#6A9FD1] checked:focus:bg-[#6A9FD1] shadow-inner"
         :id="choice.id"
         :name="question"
       />
--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="mt-96 height-48">
-    <SurveyButton :form="form" :name="question" />
+  <div class="mt-32 height-48 text-2xl ">
+    <fieldset class="field">
+    <legend >{{ question }}</legend>
+    <div v-for="choice in form" :key="choice" class="circle-button flex flex-wrap w-[30%] p-[0%] m-[0%]">
+      <input
+        type="radio"
+        class="w-6 h-6 m-2 outline-1 checked:border-1 checked:border-black outline-black checked:outline-black checked:outline-1 checked:outline-offset-0 bg-[#EAEAEA] focus:ring-transparent checked:bg-none checked:bg-[#6A9FD1] checked:hover:bg-[#6A9FD1] checked:focus:bg-[#6A9FD1] shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]"
+        :id="choice.id"
+        :name="question"
+      />
+      <label :for="choice.id">{{ choice.answer }}</label>
+    </div>
+  </fieldset>
   </div>
 </template>
 
