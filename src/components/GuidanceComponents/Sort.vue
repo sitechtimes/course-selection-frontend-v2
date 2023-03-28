4f9ee129ffa418b6918bcc4b5c8d84f2b20aebc4
AgnethaDMello <agnethad@nycstudents.net>
2023-03-28 12:29:17 -0400

    table fixed and done!

---
+++ src/components/GuidanceComponents/Sort.vue
@@ -0,0 +1,39 @@
+<template>
+    <div></div>
+<!--     <div
+      class="w-64 h-10 mb-4 relative border border-zinc-300 bg-white text-zinc-400 rounded cursor-pointer"
+      id="sort"
+      @click="isOpen = !isOpen"
+    >
+      <DownArrow
+        class="absolute fill-zinc-400 right-2 top-[0.6rem] text-zinc-400 z-0 cursor-pointer"
+      />
+      <div class="w-40">
+        <a class="flex justify-left mt-2 ml-3">
+          <p class="font-bold" id="sortshow">{{ selected }}</p>
+        </a>
+      </div>
+      <div class="sub-menu" v-if="isOpen">
+        <div
+          @click="SubjectSort()"
+          class="flex justify-left h-10 p-1 border border-zinc-300 bg-white text-zinc-400"
+        >
+          <button class="ml-2">
+            {{ subject }}
+          </button>
+        </div>
+        <div
+          @click="IncNameSort()"
+          class="flex justify-left h-10 p-1 border border-t-transparent border-zinc-300 bg-white text-zinc-400"
+        >
+          <button class="ml-2">{{ atoz }}</button>
+        </div>
+        <div
+          @click="DecNameSort()"
+          class="flex justify-left h-10 p-1 border border-t-transparent border-zinc-300 bg-white text-zinc-400"
+        >
+          <button class="ml-2">{{ ztoa }}</button>
+        </div>
+      </div>
+    </div> -->
+</template>
--- src/views/Guidance-StudentList.vue
+++ src/views/Guidance-StudentList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import SearchBar from '../components/GeneralComponents/SearchBar.vue';
+import SearchBar from '../components/GuidanceComponents/SearchBar.vue';
 import DownArrow from '../components/icons/DownArrow.vue';
+import Sort from '../components/GuidanceComponents/Sort.vue';
 
 const students = [
     {
@@ -25,7 +26,7 @@ const students = [
         grade: 11,
         osis: 93453927859,
         email: "jessiey2@nycstudents.net",
-        progress: "Completed",
+        progress: "Complete",
     },
     {
         lastname: "Whalen",
@@ -50,7 +51,7 @@ const students = [
         <SearchBar class="" type="text" placeholder="Search Students..." />
         </div>
     <div class="flex flex-col mt-10 justify-center items-center" id="table">
-<!--         <table class="w-[100rem] border-2 border-black table-auto text-left">
+        <table class="w-[100rem] border-2 border-black table-auto text-left">
             <thead class="fill-primary-g border-2 border-black">
                 <tr class="bg-primary-g">
                     <th class="p-4">Name</th>
@@ -61,60 +62,20 @@ const students = [
                     <th class="p-4">Details</th>
                 </tr>
             </thead>
-            <tbody class="border-2 border-black" v-for="student for students">
+            <tbody v-for="student in students" class="border-2 border-black">
                 <tr>
-                    <td>{{ osis }}</td>
-                </tr>
-            </tbody>
-        </table> -->
-         <table class="w-[100rem] border-2 border-black table-auto text-left">
-            <thead class="fill-primary-g border-2 border-black">
-                <tr class="bg-primary-g">
-                    <th class="p-4">Name</th>
-                    <th class="p-4">Grade</th>
-                    <th class="p-4">OSIS</th>
-                    <th class="p-4">Email</th>
-                    <th class="p-4">Progress</th>
-                    <th class="p-4">Details</th>
-                </tr>
-            </thead>
-            <tbody class="border-2 border-black">
-                <tr>
-                    <td class="p-4">Lin, Wendy</td>
-                    <td class="p-4">10</td>
-                    <td class="p-4">93453927859</td>
-                    <td class="p-4">wendyl26@nycstudents.net</td>
-                    <td class="p-4">
-                        <p class="text-[#461616] bg-[#EA9F9F] w-[8rem] font-semibold text-center p-1 rounded-2xl">Not Started</p>
+                    <td class="p-4">{{ student.lastname }}, {{ student.firstname }}</td>
+                    <td class="p-4">{{ student.grade }}</td>
+                    <td class="p-4">{{ student.osis }}</td>
+                    <td class="p-4">{{ student.email }}</td>
+                    <td class="p-4" v-if="student.progress === 'Not Started'">
+                        <p class="text-[#461616] bg-[#EA9F9F] w-[8rem] font-semibold text-center p-1 rounded-2xl">{{ student.progress }}</p>
                     </td>
-                    <td class="p-4">View</td>
-                </tr>
-                <tr class="border-2 border-black">
-                    <td class="p-4">Chen, Jason</td>
-                    <td class="p-4">12</td>
-                    <td class="p-4">828471831</td>
-                    <td class="p-4">jasonc2801@nycstudents.net</td>
-                    <td class="p-4">
-                        <p class="text-[#461616] bg-[#F9D477] w-[8rem] font-semibold text-center p-1 rounded-2xl">In Progress</p>
+                    <td class="p-4" v-else-if="student.progress === 'In Progress'">
+                        <p class="text-[#461616] bg-[#F9D477] w-[8rem] font-semibold text-center p-1 rounded-2xl">{{ student.progress }}</p>
                     </td>
-                    <td class="p-4">View</td>
-                </tr>
-                <tr class="border-2 border-black">
-                    <td class="p-4">Yang, Jessie</td>
-                    <td class="p-4">11</td>
-                    <td class="p-4">93453927859</td>
-                    <td class="p-4">jessiey2@nycstudents.net</td>
-                    <td class="p-4">
-                        <p class="text-[#174616] bg-[#A8D480] w-[8rem] font-semibold text-center p-1 rounded-2xl">Complete</p></td>
-                    <td class="p-4">View</td>
-                </tr>
-                <tr class="border-2 border-black">
-                    <td class="p-4">Whalen, Michael</td>
-                    <td class="p-4">9</td>
-                    <td class="p-4">828471831</td>
-                    <td class="p-4">mwhalen21@nycdoe.gov</td>
-                    <td class="p-4">
-                        <p class="text-[#461616] bg-[#EA9F9F] w-[8rem] font-semibold text-center p-1 rounded-2xl">Not Started</p>
+                    <td class="p-4" v-else-if="student.progress === 'Complete'">
+                        <p class="text-[#174616] bg-[#A8D480] w-[8rem] font-semibold text-center p-1 rounded-2xl">{{ student.progress }}</p>
                     </td>
                     <td class="p-4">View</td>
                 </tr>
