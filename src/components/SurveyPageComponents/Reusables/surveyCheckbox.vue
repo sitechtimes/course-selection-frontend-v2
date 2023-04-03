e9a66a558abc225c07146ba044ab791ca1829941
wrrius <2.dan.0.bay.0@gmail.com>
2023-04-03 12:29:22 -0400

    not ddonme!

--- src/components/SurveyPageComponents/Reusables/surveyCheckbox.vue
+++ src/components/SurveyPageComponents/Reusables/surveyCheckbox.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="md:mt-32 mt-16 md:text-4xl text-2xl flex flex-wrap flex-row md:ml-32 ">
-      <fieldset class="field flex" :class="height"> <!-- I did this because I have no idea how to make it fill the rest of the page, so this will let the page decide how long it should be.  -->
+    <div class="md:text-4xl text-2xl flex flex-wrap flex-row md:ml-32">
+      <fieldset class="field flex overflow-visible" :class="height"> <!-- I did this because I have no idea how to make it fill the rest of the page, so this will let the page decide how long it should be.  -->
       <legend class="flex flex-wrap overflow-visible">{{ question }}</legend>
-      <div class="field flex flex-col flex-wrap">
-        <div v-for="choice in classes" :key="choice" class="square-button flex flex-wrap flex-column m-4 md:ml-8">
+      <div class="field flex flex-col flex-wrap w-fit ">
+        <div v-for="choice in classes" :key="choice" class="square-button flex flex-wrap flex-column m-4 md:ml-8 ">
           <input
             type="checkbox"
-            class="w-6 h-6 outline-1 self-center flex flex-wrap flex-row checked:border-1 checked:border-black outline-black checked:outline-black checked:outline-1 checked:outline-offset-0 bg-[#EAEAEA] focus:ring-transparent checked:bg-none checked:bg-[#6A9FD1] checked:hover:bg-[#6A9FD1] checked:focus:bg-[#6A9FD1] shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]"
+            class="w-6 h-6 outline-1 self-center flex flex-wrap flex-row checked:border-1 checked:border-black outline-black checked:outline-black checked:outline-1 checked:outline-offset-0 bg-[#EAEAEA] focus:ring-transparent checked:bg-none checked:bg-[#6A9FD1] checked:hover:bg-[#6A9FD1] checked:focus:bg-[#6A9FD1] shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25) "
             :id="choice.id"
             :name="question"
           />
-          <label :for="choice.id" class="flex flex-wrap self-center md:ml-6 flex-row">{{ choice.answer }}</label>
+          <label :for="choice.id" class="flex flex-wrap self-center md:ml-6 flex-row ">{{ choice.answer }}</label>
         </div>
       </div>
       </fieldset>
--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,14 +1,21 @@
 <template>
-  <surveyCheckbox :classes="classes" :question="question" :height="height"></surveyCheckbox>
-  <div class="border-black border-[0.5px] border-solid rounded-xl w-[40%]">
-    <p>Drag course(s) into order of preference:</p>
-    <div class="bg-[#D6EEFF] w-[22rem] h-[5rem] flex justify-center items-center shadow-[4px_3px_3px_rgba(0,0,0,0.3)] rounded-2xl">                
-      <p class="text-black text-[1.3rem]">Basketball Elective</p>
+  <section class="mt-[7rem] md:text-left text-center">
+    <h1 class="text-[#37394F] text-4xl md:text-6xl font-bold text-center">______ Year Survey</h1>     <!-- Fill in ______ with the year -->
+    <div class="flex">
+      <surveyCheckbox :classes="classes" :question="question" :height="height" class="md:mt-8 mt-16"></surveyCheckbox>
+      <div class="border-black border-[0.5px] border-solid rounded-xl w-[20%] ml-20 h-fit md:mt-8 mt-16">
+        <p class="text-[1.5rem] text-black flex justify-self-center	">Drag course(s) into order of preference:</p>
+        <div class="flex justify-center">
+          <div class="bg-[#D6EEFF] w-[22rem] h-[5rem] flex justify-center items-center shadow-[4px_3px_3px_rgba(0,0,0,0.3)] rounded-2xl ">                
+            <p class="text-[#37394F] text-[1.3rem] font-bold">AP Psychology</p>
+          </div>
+        </div>
+        <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2rem]">
+          Confirm
+        </button>
+      </div>
     </div>
-    <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2rem]">
-      Confirm
-    </button>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -18,59 +25,47 @@ import backButton from "../components/SurveyPageComponents/reusables/backButton.
 import surveyCheckbox from "../components/SurveyPageComponents/Reusables/surveyCheckbox.vue";
 
 
-const height = "h-[30rem]"
+const height = "h-[40rem]"
 
 const classes = [
   {
     id: "first-class",
-    answer: "class 1",
+    answer: "Biology (non-AP)",
   },
   {
     id: "second-class",
-    answer: "class 2",
+    answer: "AP Biology",
   },
   {
     id: "third-class",
-    answer: "class 3",
+    answer: "AP Psychology",
   },
   {
-    id: "first-class",
-    answer: "class 1",
+    id: "fourth-class",
+    answer: "AP Chemistry",
   },
   {
-    id: "second-class",
-    answer: "class 2",
+    id: "fifth-class",
+    answer: "AP Physics 1",
   },
   {
-    id: "third-class",
-    answer: "class 3",
+    id: "sixth-class",
+    answer: "AP Physics 2",
   },
   {
-    id: "first-class",
-    answer: "class 1",
+    id: "seventh-class",
+    answer: "AP Physics C",
   },
   {
-    id: "second-class",
-    answer: "class 2",
+    id: "eighth-class",
+    answer: "Applied Physics",
   },
   {
-    id: "third-class",
-    answer: "class 3",
-  },
-  {
-    id: "first-class",
-    answer: "class 1",
-  },
-  {
-    id: "second-class",
-    answer: "class 2",
-  },
-  {
-    id: "third-class",
-    answer: "class 3",
+    id: "ninth-class",
+    answer: "Anatomy and Physiology",
   },
 ];
-const question = "Are you interested in a 8th academic period (1-9 schedule)?";
+const question = "Select the Science course(s) you are interested in:";
 </script>
 
 <style scoped></style>
