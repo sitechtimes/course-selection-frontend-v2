e2cd3a884c4c4ee5df33d753bb73c23598004ff8
wrrius <2.dan.0.bay.0@gmail.com>
2023-04-17 12:29:21 -0400

    percent not working on that div

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -3,16 +3,20 @@
     <h1 class="text-[#37394F] text-4xl md:text-6xl font-bold text-center">______ Year Survey</h1>     <!-- Fill in ______ with the year -->
     <div class="flex">
       <surveyCheckbox :classes="classes" :question="question" :height="height" class="md:mt-8 mt-16"></surveyCheckbox>
-      <div class="border-black border-[0.5px] border-solid rounded-xl w-[20%] ml-20 h-fit md:mt-8 mt-16">
-        <p class="text-[1.5rem] text-black flex justify-self-center	">Drag course(s) into order of preference:</p>
-        <div class="flex justify-center">
-          <div class="bg-[#D6EEFF] w-[22rem] h-[5rem] flex justify-center items-center shadow-[4px_3px_3px_rgba(0,0,0,0.3)] rounded-2xl ">                
-            <p class="text-[#37394F] text-[1.3rem] font-bold">AP Psychology</p>
+      <div class="border-black border-[0.5px] border-solid rounded-xl w-[40%] ml-20 h-3/5 md:mt-8 mt-16 ">
+        <div class="flex justify-center mt-8">
+          <p class="text-[1.5rem] text-black w-80">Drag course(s) into order of preference:</p>
+        </div>
+        <div class="flex justify-center mt-4">
+          <div class="bg-[#D6EEFF] w-[18rem] h-[3rem] flex justify-center items-center shadow-[4px_3px_3px_rgba(0,0,0,0.3)] rounded-2xl ">                
+            <p class="text-[#37394F] text-[1.5rem] font-bold">AP Psychology</p>
           </div>
         </div>
-        <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2rem]">
-          Confirm
-        </button>
+        <div class="flex flex-row-reverse">
+          <button class="bg-[#6A9FD1] text-white w-[30%] h-[3.5rem] text-[1.5rem] md:w-[10rem] md:text-[2rem] mr-5 mb-5">
+            Confirm
+          </button>
+        </div>
       </div>
     </div>
   </section>
