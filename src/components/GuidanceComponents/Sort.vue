f54c13ebe36bd0b745586aba6edc03d937b7e0de
AgnethaDMello <agnethad@nycstudents.net>
2023-04-19 12:21:27 -0400

    push

--- src/components/GuidanceComponents/Sort.vue
+++ src/components/GuidanceComponents/Sort.vue
@@ -15,26 +15,6 @@ const grade9 = ref("Grade 9")
 const grade10 = ref("Grade 10")
 const grade11 = ref("Grade 11")
 
-const IncNameSort = computed(() => {
-  function compare(a, b) {
-    if (a.lastname < b.lastname) return -1;
-    if (a.lastname > b.lastname) return 1;
-    return 0;
-  }
-  console.log(students.sort(compare))
-  return students.sort(compare);
-});
-
-const DecNameSort = computed(() => {
-  function compare(a, b) {
-    if (a.lastname > b.lastname) return -1;
-    if (a.lastname < b.lastname) return 1;
-    return 0;
-  }
-  console.log(students.sort(compare))
-  return students.sort(compare);
-});
-
 function show() {
   onMounted(() => {
     return {
@@ -74,7 +54,7 @@ function show() {
 </script>
 
 <template>
-  <div class="w-[16.4rem]">
+     <div class="w-[16.4rem]">
      <div
       class="h-10 mr-4 flex flex-row bg-primary-g text-black justify-evenly cursor-pointer shadow-[4px_3px_3px_rgba(0,0,0,0.25)]"
       id="sort"
@@ -89,49 +69,41 @@ function show() {
       </div>
       <div class="sub-menu absolute shadow-[4px_3px_3px_rgba(0,0,0,0.25)]" v-if="isOpen">
         <div
-          @click="IncNameSort()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ atoz }}</button>
         </div>
         <div
-          @click="DecNameSort()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ ztoa }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ notStarted }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ inProgress }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ complete }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ grade9 }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ grade10 }}</button>
         </div>
         <div
-          @click="e()"
           class="flex justify-left h-10 w-44 p-1 border border-t-transparent border-primary-g bg-tertiary-g"
         >
           <button class="ml-2">{{ grade11 }}</button>
