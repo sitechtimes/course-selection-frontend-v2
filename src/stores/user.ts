ea03119129e929ca246ad596178514f2bb1d2933
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-05-01 12:15:29 -0400

    Update user.ts

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -78,6 +78,7 @@ export const useUserStore = defineStore('user', {
                             coursesAvailable{
                                 courseCode
                             }
+                            meeting
                         }
                         survey{
                             questions{
