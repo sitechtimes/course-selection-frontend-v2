5ad8690403448dbacc070c7b04d2794c7878087e
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2023-05-02 12:03:30 -0400

    Update MobileNav.vue

--- src/components/NavbarComponents/MobileNav.vue
+++ src/components/NavbarComponents/MobileNav.vue
@@ -6,20 +6,18 @@ import { useUserStore } from '../../stores/user';
 
 const userStore = useUserStore()
 
-const redirect = () => {
-    if (userStore.isLoggedIn === true) {
-        if (userStore.userType === 'student') {
-            return '/student/dashboard'
-        } 
-        if (userStore.userType === 'guidance') {
-            return '/guidance/dashboard'
-        }
-    } else {
-        return '/'
-    }
-}
-
-const link = ref(null)
+// const redirect = () => {
+//     if (userStore.isLoggedIn === true) {
+//         if (userStore.userType === 'student') {
+//             return '/student/dashboard'
+//         } 
+//         if (userStore.userType === 'guidance') {
+//             return '/guidance/dashboard'
+//         }
+//     } else {
+//         return '/'
+//     }
+// }
 
 onMounted(() => {
     const tl = gsap.timeline({
@@ -29,7 +27,7 @@ onMounted(() => {
             }
         })
 
-        tl.from(link.value, {
+        tl.from("#link", {
             x: -100, 
             opacity: 0,
             stagger: 0.2,
@@ -41,15 +39,15 @@ onMounted(() => {
     <div id="menu" class="fixed inset-0 h-screen w-full  ease-in duration-200 md:hidden z-30">
         <div id="overlay" class="absolute inset-0 w-full h-full bg-white opacity-100 -z-10"></div>
         <div v-if="userStore.isLoggedIn" class="h-full absolute top-40 left-16 flex flex-col justify-start items-start space-y-8 z-10">
-            <RouterLink @click="$emit('e')" ref="link" to="/"><p class="text-4xl" >Home</p></RouterLink>
-            <RouterLink @click="$emit('e')" ref="link" to="/survey"><p class="text-4xl">Survey</p></RouterLink>
-            <RouterLink @click="$emit('e')" ref="link" to="/"><p class="text-4xl">Courses</p></RouterLink>
-            <p ref="link" @click="$emit('e'), userStore.isLoggedIn = false" class="text-4xl text-red-500 cursor-pointer">Logout</p>
+            <RouterLink @click="$emit('e')" id="link" to="/"><p class="text-4xl" >Home</p></RouterLink>
+            <RouterLink @click="$emit('e')" id="link" to="/survey"><p class="text-4xl">Survey</p></RouterLink>
+            <RouterLink @click="$emit('e')" id="link" to="/"><p class="text-4xl">Courses</p></RouterLink>
+            <p id="link" @click="$emit('e'), userStore.isLoggedIn = false" class="text-4xl text-red-500 cursor-pointer">Logout</p>
         </div>
         <div v-else class="h-full absolute top-56 left-16 flex flex-col justify-start items-start space-y-8 z-10">
-            <RouterLink @click="$emit('e')" ref="link" to="/"><p class="text-4xl">Home</p></RouterLink>
-            <RouterLink @click="$emit('e')" ref="link" to="/courses"><p class="text-4xl">Courses</p></RouterLink>
-            <RouterLink @click="$emit('e')" ref="link" to="/login"><p class="text-4xl">Login</p></RouterLink>
+            <RouterLink @click="$emit('e')" id="link" to="/"><p class="text-4xl">Home</p></RouterLink>
+            <RouterLink @click="$emit('e')" id="link" to="/courses"><p class="text-4xl">Courses</p></RouterLink>
+            <RouterLink @click="$emit('e')" id="link" to="/login"><p class="text-4xl">Login</p></RouterLink>
         </div>
     </div>
 </template>
