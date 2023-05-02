91f4756f65364145001201d260cf9ddf91e5646c
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-05-02 12:15:52 -0400

    -

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,41 +1,15 @@
 <template>
-  <!-- Page 1 (for everyone)
-    <h1>______ Year Survey</h1>     Fill in ______ with the year
-    <label for="major">What college majors are you considering upon graduation from high school?</label>
-    <input type="text" id="major"/>
-    <label for="career">What careers are you considering upon graduation?</label>
-    <input type="text" id="career"/>
-    <label for="afterSchool">List after-school activities such as clubs, teams, volunteer, or paid employment.</label>
-    <input type="text" id="afterSchool"/> -->
 
-  <!-- Page 2 (for everyone) -->
-  <!-- <div class="survey">
-    <h1 class="text-4xl font-semibold">______ Year Survey</h1>
-    <fieldset class="flex flex-col">
-      <label for="AP-interested" class=""
-        >How Many AP or college-level courses are you interested in
-        taking?</label
-      >
-      <input type="text" class="border-2 border-gray-500 rounded-sm w-4/5" id="AP-interested" />
-    </fieldset>
-
-    <h2>Are you interested in a 8th academic period (1-9 schedule)?</h2>
-    <fieldset>
-      <input type="radio" id="eighth-yes" name="eighth" />
-      <label for="eighth-yes">Yes</label>
-      <input type="radio" id="eighth-no" name="eighth" />
-      <label for="eighth-no">No</label>
-      <input type="radio" id="eighth-ns" name="eighth" />
-      <label for="eighth-ns">Not sure</label>
-    </fieldset>
-  </div> -->
   <div class="mt-96 height-48">
     <h1>ok</h1>
-    <VueDraggableNext v-model="courses" @change="log">
-      <ul v-for="course in courses" :key="course" class="text-xl">
-        {{ course }}
-      </ul>
-    </VueDraggableNext>
+
+    <draggable v-model="courses" group="courses" item-key="name" @start="log" @end="log">
+      <template #item="{ element }">
+        <div>
+          {{ element.name }}
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
@@ -43,9 +17,10 @@
 
 <script setup lang="ts">
 import SurveyButton from "../components/SurveyPageComponents/SurveyButton.vue";
-import {VueDraggableNext} from "vue-draggable-next";
+import draggable from "vuedraggable";
 
 // test stuff
+let drag = false
 const form = [
   {
     id: "first-question",
@@ -57,17 +32,46 @@ const form = [
   },
 ];
 const courses = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",  
-  "F",
-  "G",
+  {
+    name: "A",
+    order: 0
+  },
+  {
+    name: "B",
+    order: 1
+  },
+  {
+    name: "C",
+    order: 2
+  },
+  {
+    name: "D",
+    order: 3
+  },
+  {
+    name: "E",
+    order: 4
+  },
+  {
+    name: "F",
+    order: 5
+  },
+  {
+    name: "G",
+    order: 6
+  },
 ]
+console.log(courses)
 const name = "eighth";
 
 function log(){
   console.log(courses)
+}
+
+function onChange() {
+    reorder()
+}
+function reorder() {
+    courses.forEach((item, index) => (item.order = index))
 }
 </script>
