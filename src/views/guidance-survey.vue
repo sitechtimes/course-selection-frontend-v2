df525451c24479d7f2327ef9abe04d0ac3511ef3
wrrius <2.dan.0.bay.0@gmail.com>
2023-05-09 12:31:01 -0400

    found survey answers for the student and questions added

--- src/views/guidance-survey.vue
+++ src/views/guidance-survey.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
 import { useUserStore } from '../stores/user'
+import booleanComponent from '../components/SurveyPageComponents/Reusables/surveyBoolean.vue'
+import generalComponent from '../components/SurveyPageComponents/Reusables/surveyGeneral.vue'
 
 const userStore = useUserStore()
 const viewedUser = userStore.data.guidance.students.filter(student => student.osis === window.location.pathname.substring(17))[0]
-console.log(userStore.data.allSurveys)
-console.log(userStore.data)
-console.log(viewedUser.homeroom[1])
+const surveyAnswers = userStore.data.allAnsweredSurveys.edges.filter(survey => survey.node.osis === viewedUser.osis)
+let usedSurvey = null
 
-if (viewedUser.homeroom[1] === 5) {
-  const usedSurvey = userStore.data.allSurveys[1]
-} else if (viewedUser.homeroom[1] === 4) {
-  const usedSurvey = userStore.data.allSurveys[2]
-} else if (viewedUser.homeroom[1] === 6) {
-  const usedSurvey = userStore.data.allSurveys[0]
+if (viewedUser.homeroom[1] === "5") {
+  usedSurvey = userStore.data.allSurveys.edges[1]
+} else if (viewedUser.homeroom[1] === "4") {
+  usedSurvey = userStore.data.allSurveys.edges[0]
+} else if (viewedUser.homeroom[1] === "6") {
+  usedSurvey = userStore.data.allSurveys.edges[2]
 }
-
+console.log(userStore.data)
 </script>
 
 <template>
-  <section>
-    <h1>Yello!</h1>
+  <section v-for="question in usedSurvey.node.questions">
+    <booleanComponent v-if="question.questionType === 'BOOLEAN'" :question="question.question"></booleanComponent>
+    <generalComponent v-if="question.questionType === 'GENERAL'" :question="question.question"></generalComponent>
   </section>
 </template>
