11b661f4be589f59932f0f8eceac8e2165a84cdb
wrrius <2.dan.0.bay.0@gmail.com>
2023-05-10 12:31:14 -0400

    making button and updated questiion

--- src/views/guidance-survey.vue
+++ src/views/guidance-survey.vue
@@ -8,19 +8,25 @@ const viewedUser = userStore.data.guidance.students.filter(student => student.os
 const surveyAnswers = userStore.data.allAnsweredSurveys.edges.filter(survey => survey.node.osis === viewedUser.osis)
 let usedSurvey = null
 
-if (viewedUser.homeroom[1] === "5") {
+if (viewedUser.grade === "JUNIOR") {
   usedSurvey = userStore.data.allSurveys.edges[1]
-} else if (viewedUser.homeroom[1] === "4") {
+} else if (viewedUser.grade === "SENIOR") {
   usedSurvey = userStore.data.allSurveys.edges[0]
-} else if (viewedUser.homeroom[1] === "6") {
+} else if (viewedUser.grade === "SOPHOMORE") {
   usedSurvey = userStore.data.allSurveys.edges[2]
 }
 console.log(userStore.data)
 </script>
 
 <template>
-  <section v-for="question in usedSurvey.node.questions">
-    <booleanComponent v-if="question.questionType === 'BOOLEAN'" :question="question.question"></booleanComponent>
-    <generalComponent v-if="question.questionType === 'GENERAL'" :question="question.question"></generalComponent>
+  <section>
+    <h1>{{ viewedUser.user.firstName }} {{ viewedUser.user.lastName }}'s Survey</h1>
+    <div v-for="question in usedSurvey.node.questions">
+      <booleanComponent v-if="question.questionType === 'BOOLEAN'" :question="question.question"></booleanComponent>
+      <generalComponent v-if="question.questionType === 'GENERAL'" :question="question.question"></generalComponent>
+    </div>
+    <div class="flex justify-center">
+      <button class="bg-[#C5D4A4] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-32 h-12 text-5xl text-[#37394F]">Complete</button>
+    </div>
   </section>
 </template>
