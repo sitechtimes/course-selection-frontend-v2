ed17b062b2ec49ab3c41c77e4ac7e5c84e53dc9f
AgnethaDMello <agnethad@nycstudents.net>
2023-05-10 12:00:46 -0400

    media queries

--- src/components/GuidanceComponents/StudentTable.vue
+++ src/components/GuidanceComponents/StudentTable.vue
@@ -16,7 +16,7 @@ const input = ref("")
       });
 }) */
 
-const search = document.getElementById('search');
+/* const search = document.getElementById('search');
 search.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
     const filteredStudents = newstudents.filter((student: any) => {
@@ -24,14 +24,15 @@ search.addEventListener('keyup', (e) => {
             student.toLowerCase().includes(searchString)
         )
     })
-})
+    showStudents(filteredStudents)
+}) */
 
 </script>
 
 <template>
-    <div class="flex flex-col mt-10 justify-center items-center" id="table">
+    <div class="flex flex-col mt-10 justify-center items-center w-5/6" id="table">
         <SearchBar id="search" class="" type="text" v-model="input" placeholder="Search Students..." />
-        <table class="w-[100rem] border-2 border-black table-auto text-left">
+        <table class="w-full border-2 border-black table-auto text-left">
             <thead class="fill-primary-g border-2 border-black">
                 <tr class="bg-primary-g">
                     <th class="p-4">Name</th>
@@ -47,7 +48,7 @@ search.addEventListener('keyup', (e) => {
                     <td class="p-4">{{ student.user.lastName }}, {{ student.user.firstName }}</td>
                     <td class="p-4">{{ student.grade }}</td>
                     <td class="p-4">{{ student.osis }}</td>
-                    <td class="p-4">{{ student.email }}</td>
+                    <td class="p-4">{{ student.user.email }}</td>
                     <td class="p-4" v-if="student.progress === 'Not Started'">
                         <p class="text-[#461616] bg-[#EA9F9F] w-[8rem] font-semibold text-center p-1 rounded-2xl">{{ student.progress }}</p>
                     </td>
