57cd14c11df57dd3822647c430d2a17ae53d8adb
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-05-14 11:47:22 -0400

    Update surveyBoolean.vue

--- src/components/SurveyPageComponents/Reusables/surveyBoolean.vue
+++ src/components/SurveyPageComponents/Reusables/surveyBoolean.vue
@@ -1,31 +1,50 @@
 <template>
     <div class="flex items-center justify-center">
-      <fieldset class="flex items-center justify-start">
+      <fieldset class="flex items-center justify-start" ref="form">
       <legend class="text-xl md:text-2xl">{{ question }}</legend>
       <div class=" flex flex-row">
-        <div v-for="choice in choices" :key="choice" class="flex justify-center items-center flex-wrap m-4">
+        <div class="flex justify-center items-center flex-wrap m-4">
           <input
             type="radio"
             class="w-4 h-4 text-blue-400 bg-zinc-100 border-gray-300 focus:ring-transparent"
-            :id="choice"
+            id="Yes"
+            value="Yes"
             :name="question"
+            v-model="response"
           />
-          <label :for="choice" class="text-lg md:text-xl ml-4">{{ choice }}</label>
+          <label for="Yes">Yes</label>
+        </div>
+        <div class="flex justify-center items-center flex-wrap m-4">
+          <input
+            type="radio"
+            class="w-4 h-4 text-blue-400 bg-zinc-100 border-gray-300 focus:ring-transparent"
+            id="No"
+            value="No"
+            :name="question"
+            v-model="response"
+          />
+          <label for="No">No</label>
         </div>
       </div>
-      </fieldset>
       <div class="bottom-28 w-11/12 md:w-4/5 lg:w-3/4 absolute flex justify-between items-center px-4">
-        <button @click="$emit('back')" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:bg-stone-400" :disabled="min">Back</button>
-        <button @click="$emit('next')" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md  disabled:bg-stone-400" :disabled="max">Next</button>
+        <button @click="$emit('back', [response]); clear()" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:bg-stone-400" type="reset" :disabled="min">Back</button>
+        <button @click="$emit('next', [response]); clear()" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md  disabled:bg-stone-400" :disabled="max">Next</button>
       </div>
+      </fieldset>
     </div>
   </template>
   
   <script setup lang="ts">
-  const choices = ["Yes", "No"]
+  import { ref, Ref, onMounted } from 'vue';
   defineProps({
     question: String,
     min: Boolean,
     max: Boolean
   });
+
+  let response: String | undefined
+
+  const clear = () => {
+    response = ''
+  }
   </script>
