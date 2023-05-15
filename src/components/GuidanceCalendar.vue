72186f5fcb981708d9d9232d7b62642f0b321015
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-05-14 23:07:45 -0400

    calendar css

--- src/components/GuidanceCalendar.vue
+++ src/components/GuidanceCalendar.vue
@@ -1,75 +1,75 @@
 <script setup lang="ts">
-const daysTag = document.querySelector(".days");
-const currentMonth = document.querySelector(".month");
-const currentYear = document.querySelector(".year");
-const prevNextIcon = document.querySelectorAll(".icons span");
-// getting new date, current year and month
-let date = new Date();
-let currYear = date.getFullYear();
-let currMonth = date.getMonth();
-// storing full name of all months in array
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-  let liTag = "";
-  for (let i = firstDayofMonth; i > 0; i--) {
-    // creating li of previous month last days
-    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-  }
-  for (let i = 1; i <= lastDateofMonth; i++) {
-    // creating li of all days of current month
-    // adding active class to li if the current day, month, and year matched
-    let isToday =
-      i === date.getDate() &&
-      currMonth === new Date().getMonth() &&
-      currYear === new Date().getFullYear()
-        ? "active"
-        : "";
-    liTag += `<li class="${isToday}">${i}</li>`;
-  }
-  for (let i = lastDayofMonth; i < 6; i++) {
-    // creating li of next month first days
-    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
-  }
-  //   currentMonth.innerText = `${months[currMonth]}`;
-  //   currentYear.innerText = `${currYear}`;
-  //   daysTag.innerHTML = liTag;
-};
-renderCalendar();
-prevNextIcon.forEach((icon) => {
-  // getting prev and next icons
-  icon.addEventListener("click", () => {
-    // adding click event on both icons
-    // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-    if (currMonth < 0 || currMonth > 11) {
-      // if current month is less than 0 or greater than 11
-      // creating a new date of current year & month and pass it as date value
-      date = new Date(currYear, currMonth, new Date().getDate());
-      currYear = date.getFullYear(); // updating current year with new date year
-      currMonth = date.getMonth(); // updating current month with new date month
-    } else {
-      date = new Date(); // pass the current date as date value
-    }
-    renderCalendar(); // calling renderCalendar function
-  });
-});
+// const daysTag = document.querySelector(".days");
+// const currentMonth = document.querySelector(".month");
+// const currentYear = document.querySelector(".year");
+// const prevNextIcon = document.querySelectorAll(".icons span");
+// // getting new date, current year and month
+// let date = new Date();
+// let currYear = date.getFullYear();
+// let currMonth = date.getMonth();
+// // storing full name of all months in array
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+// const renderCalendar = () => {
+//   let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+//     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+//     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+//     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+//   let liTag = "";
+//   for (let i = firstDayofMonth; i > 0; i--) {
+//     // creating li of previous month last days
+//     liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+//   }
+//   for (let i = 1; i <= lastDateofMonth; i++) {
+//     // creating li of all days of current month
+//     // adding active class to li if the current day, month, and year matched
+//     let isToday =
+//       i === date.getDate() &&
+//       currMonth === new Date().getMonth() &&
+//       currYear === new Date().getFullYear()
+//         ? "active"
+//         : "";
+//     liTag += `<li class="${isToday}">${i}</li>`;
+//   }
+//   for (let i = lastDayofMonth; i < 6; i++) {
+//     // creating li of next month first days
+//     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+//   }
+//   //   currentMonth.innerText = `${months[currMonth]}`;
+//   //   currentYear.innerText = `${currYear}`;
+//   //   daysTag.innerHTML = liTag;
+// };
+// renderCalendar();
+// prevNextIcon.forEach((icon) => {
+//   // getting prev and next icons
+//   icon.addEventListener("click", () => {
+//     // adding click event on both icons
+//     // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+//     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+//     if (currMonth < 0 || currMonth > 11) {
+//       // if current month is less than 0 or greater than 11
+//       // creating a new date of current year & month and pass it as date value
+//       date = new Date(currYear, currMonth, new Date().getDate());
+//       currYear = date.getFullYear(); // updating current year with new date year
+//       currMonth = date.getMonth(); // updating current month with new date month
+//     } else {
+//       date = new Date(); // pass the current date as date value
+//     }
+//     renderCalendar(); // calling renderCalendar function
+//   });
+// });
 </script>
 <template>
   <div class="container">
@@ -92,48 +92,48 @@ prevNextIcon.forEach((icon) => {
         <li class="border-x-2 px-11">Sat</li>
       </ul>
       <ul class="days">
-        <li>26</li>
-        <li>27</li>
-        <li>28</li>
-        <li>29</li>
-        <li>30</li>
-        <li>31</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>11</li>
-        <li>12</li>
-        <li>13</li>
-        <li>14</li>
-        <li>15</li>
-        <li>16</li>
-        <li>17</li>
-        <li>18</li>
-        <li>19</li>
-        <li>20</li>
-        <li>21</li>
-        <li>22</li>
-        <li>23</li>
-        <li>24</li>
-        <li>25</li>
-        <li>26</li>
-        <li>27</li>
-        <li>28</li>
-        <li>29</li>
-        <li>30</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
+        <li class="mr-4">26</li>
+        <li class="mr-4">27</li>
+        <li class="mr-4">28</li>
+        <li class="mr-4">29</li>
+        <li class="mr-4">30</li>
+        <li class="mr-4">31</li>
+        <li class="mr-4">1</li>
+        <li class="mr-4">2</li>
+        <li class="mr-4">3</li>
+        <li class="mr-4">4</li>
+        <li class="mr-4">5</li>
+        <li class="mr-4">6</li>
+        <li class="mr-4">7</li>
+        <li class="mr-4">8</li>
+        <li class="mr-4">9</li>
+        <li class="mr-4">10</li>
+        <li class="mr-4">11</li>
+        <li class="mr-4">12</li>
+        <li class="mr-4">13</li>
+        <li class="mr-4">14</li>
+        <li class="mr-4">15</li>
+        <li class="mr-4">16</li>
+        <li class="mr-4">17</li>
+        <li class="mr-4">18</li>
+        <li class="mr-4">19</li>
+        <li class="mr-4">20</li>
+        <li class="mr-4">21</li>
+        <li class="mr-4">22</li>
+        <li class="mr-4">23</li>
+        <li class="mr-4">24</li>
+        <li class="mr-4">25</li>
+        <li class="mr-4">26</li>
+        <li class="mr-4">27</li>
+        <li class="mr-4">28</li>
+        <li class="mr-4">29</li>
+        <li class="mr-4">30</li>
+        <li class="mr-4">1</li>
+        <li class="mr-4">2</li>
+        <li class="mr-4">3</li>
+        <li class="mr-4">4</li>
+        <li class="mr-4">5</li>
+        <li class="mr-4">6</li>
       </ul>
     </div>
   </div>
@@ -153,6 +153,12 @@ prevNextIcon.forEach((icon) => {
   display: flex;
   flex-direction: row;
 }
+/* .calendar ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  text-align: center;
+}
 .weeks {
   display: flex;
   flex-direction: row;
@@ -162,5 +168,88 @@ prevNextIcon.forEach((icon) => {
   width: 65%;
   border-style: solid;
   border-width: 2px;
+} */
+body {
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: center;
+  min-height: 100vh;
+  background: #9b59b6;
+}
+.wrapper {
+  width: 450px;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 15px 40px rgba(0, 0, 0, 0.12);
+}
+.wrapper header {
+  display: flex;
+  align-items: center;
+  padding: 25px 30px 10px;
+  justify-content: space-between;
+}
+.calendar {
+  padding: 20px;
+}
+.calendar ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  overflow: hidden;
+}
+.weeks {
+  text-align: center;
+}
+.calendar .days {
+  margin-bottom: 20px;
+}
+.days {
+  text-align: right;
+  margin-top: 1rem;
+}
+.calendar li {
+  color: #333;
+  width: calc(100% / 7);
+  font-size: 1.07rem;
+}
+.calendar .weeks li {
+  font-weight: 500;
+  cursor: default;
+  width: calc(100% / 7);
+}
+.calendar .days li {
+  z-index: 1;
+  cursor: pointer;
+  position: relative;
+  padding-top: 0.5rem;
+  padding-bottom: 4rem;
+  padding-right: 1rem;
+}
+.days li {
+  border: #e5e7eb 2px solid;
+}
+.days li.inactive {
+  color: #aaa;
+}
+.days li.active {
+  color: #fff;
+}
+/* .days li::before {
+  position: absolute;
+  content: "";
+  left: 94.5%;
+  top: 50%;
+  height: 40px;
+  width: 40px;
+  z-index: -1;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+} circle highlight for days */
+.days li.active::before {
+  background: #9b59b6;
+}
+.days li:not(.active):hover::before {
+  background: #f2f2f2;
 }
 </style>
