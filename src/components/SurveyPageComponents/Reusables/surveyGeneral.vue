8c74495611ba16b450e4526e2c768a09e8478b26
wrrius <2.dan.0.bay.0@gmail.com>
2023-05-22 12:31:42 -0400

    finished survey general started the drag and drop-esque thing

--- src/components/SurveyPageComponents/Reusables/surveyGeneral.vue
+++ src/components/SurveyPageComponents/Reusables/surveyGeneral.vue
@@ -5,6 +5,11 @@
         <label :for="usedSurvey.indexOf(question)" class="text-xl self-center hidden" >Student Answer:</label>
         <input :id="usedSurvey.indexOf(question)" class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base bg-transparent rounded-md border border-solid border-zinc-400  focus:outline-none focus:ring-0 focus:border-blue-400 lg:text-[180%] " type="text">
       </fieldset>
+      <fieldset v-else-if="typeof(question) === 'string'" class="flex items-center justify-start space-y-6 ">
+        <legend class="text-xl md:text-2xl lg:text-[180%] overflow-x-visible">{{ question.question }}</legend>
+        <label :for="question" class="text-xl self-center hidden" >Student Answer:</label>
+        <input :id="question" class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base bg-transparent rounded-md border border-solid border-zinc-400  focus:outline-none focus:ring-0 focus:border-blue-400 lg:text-[180%] " type="text">
+      </fieldset>
     </div>
   </template>
   
--- src/views/guidance-survey.vue
+++ src/views/guidance-survey.vue
@@ -15,7 +15,7 @@ if (viewedUser.grade === "JUNIOR") {
 } else if (viewedUser.grade === "SOPHOMORE") {
   usedSurvey = userStore.data.allSurveys.edges[2]
 }
-console.log(userStore.data)
+console.log(usedSurvey.node.questions[1].question)
 </script>
 
 <template>
@@ -29,7 +29,12 @@ console.log(userStore.data)
     <div v-for="question in usedSurvey.node.questions" class="flex justify-center">
       <booleanComponent class="mb-6 " v-if="question.questionType === 'BOOLEAN'" :question="question" :usedSurvey="usedSurvey.node.questions"></booleanComponent>
       <generalComponent class="mb-6" v-if="question.questionType === 'GENERAL'" :question="question" :usedSurvey="usedSurvey.node.questions"></generalComponent>
-      
+      <section v-if="question.questionType != 'BOOLEAN' && question.questionType != 'GENERAL'" class="flex items-center justify-start w-3/4 overflow-x-visible ">
+        <div class="flex items-center space-y-6">
+          <h1 class="text-xl md:text-2xl lg:text-[180%]">{{ question.question }}</h1>
+          <input class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base bg-transparent rounded-md border border-solid border-zinc-400  focus:outline-none focus:ring-0 focus:border-blue-400 lg:text-[180%] " type="text">
+        </div>
+      </section>
     </div>
     <div class="flex justify-center mb-6">
       <button class="bg-[#C5D4A4] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-36 h-12 text-2xl font-bold text-[#37394F]">Complete</button>
