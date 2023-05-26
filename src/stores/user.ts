6d69a06a63deff70938993f887c4240c20e1dd53
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-05-26 12:21:11 -0400

    Update user.ts

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -56,6 +56,7 @@ export const useUserStore = defineStore('user', {
                                             question
                                             questionType
                                         }
+                                        dueDate
                                     }
                                 }
                             }
@@ -111,6 +112,7 @@ export const useUserStore = defineStore('user', {
                                 question
                                 questionType
                             }
+                            dueDate
                         }
                         answeredSurvey{
                             answers
