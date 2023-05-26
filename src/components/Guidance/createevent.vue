24cd56790e8a3f56368ceabd54c3bb8240713147
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2023-05-26 12:27:51 -0400

    Update createevent.vue

--- src/components/Guidance/createevent.vue
+++ src/components/Guidance/createevent.vue
@@ -8,8 +8,9 @@ let date: String;
 let people: String;
 let description: String;
 
-function submit(date: String) {
+function submit(date: String, people: String) {
   console.log(date);
+  console.log(people);
   document.getElementById("form").reset();
   let json = JSON.stringify(date);
   userStore.changeMeeting("123", date);
@@ -72,23 +73,43 @@ function show() {
               placeholder="Title"
             />
           </div>
-          <div class="item">
-            <label class="formt" for="date">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                <path
-                  d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192H400V448c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192z"
-                />
-              </svg>
-              Date</label
-            >
-            <input
-              class="space"
-              type="date"
-              v-model="date"
-              placeholder="Date"
-            />
+          <div class="times">
+            <div class="item">
+              <label class="formt" for="date">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                  <path
+                    d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192H400V448c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192z"
+                  />
+                </svg>
+                Date</label
+              >
+              <input
+                class="space d"
+                type="date"
+                v-model="date"
+                placeholder="Date"
+              />
+            </div>
+            <div class="item">
+              <label class="formt" for="time">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                  <path
+                    d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+                  />
+                </svg>
+                Time</label
+              >
+              <input
+                class="space d"
+                type="time"
+                v-model="time"
+                placeholder="Time"
+              />
+            </div>
           </div>
+
           <div class="item">
             <label class="formt" for="emails">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -124,7 +145,11 @@ function show() {
             />
           </div>
           <div class="item submit">
-            <button @click="submit(date)" type="submit" class="font-bold">
+            <button
+              @click="submit(date, people)"
+              type="submit"
+              class="font-bold"
+            >
               Save
             </button>
           </div>
@@ -145,6 +170,13 @@ function show() {
 </template>
 
 <style scoped>
+.d {
+  width: 50%;
+}
+.times {
+  display: flex;
+  flex-direction: row;
+}
 .tab {
   border-radius: 5px;
   text-align: left;
