49253ddafad9bb5fd914474fa8241d4011fdfedf
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2023-05-31 12:29:02 -0400

    5/31/23
    we have liftoff a.k.a hashir is the best

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "../../src/stores/user";
 
+// function guardRoute() {
+//   console.log("you need to log in");
+//   console.log(localStorage.user);
+// }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/landing.vue')
+      path: "/",
+      name: "home",
+      component: () => import("../views/landing.vue"),
+      beforeEnter: guardRoute,
     },
     {
-      path: '/guidance/dashboard',
-      name: 'guidanceDash',
-      component: () => import('../views/GuidanceHome.vue')
+      path: "/guidance/dashboard",
+      name: "guidanceDash",
+      component: () => import("../views/GuidanceHome.vue"),
     },
     {
-      path: '/guidance-studentlist',
-      name: 'Guidance-Studentlist',
-      component: () => import('../views/Guidance-StudentList.vue')
+      path: "/guidance/studentlist",
+      name: "Guidance/Studentlist",
+      component: () => import("../views/Guidance-StudentList.vue"),
     },
     // {
     //   path: '/courses',
@@ -26,32 +32,45 @@ const router = createRouter({
     // },
 
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/login/index.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../views/login/index.vue"),
     },
     {
-      path: '/login/student',
-      name: 'studentLogin',
-      component: () => import('../views/login/student.vue')
+      path: "/login/student",
+      name: "studentLogin",
+      component: () => import("../views/login/student.vue"),
     },
     {
-      path: '/login/guidance',
-      name: 'guidanceLogin',
-      component: () => import('../views/login/guidance.vue')
+      path: "/login/guidance",
+      name: "guidanceLogin",
+      component: () => import("../views/login/guidance.vue"),
     },
 
     {
-      path: '/survey',
-      name: 'survey',
-      component: () => import('../views/SurveyPage.vue')
+      path: "/survey",
+      name: "survey",
+      component: () => import("../views/SurveyPage.vue"),
     },
     {
-      path: '/student/dashboard',
-      name: 'studentDash',
-      component: () => import('../views/dashboard.vue')
+      path: "/student/dashboard",
+      name: "studentDash",
+      component: () => import("../views/dashboard.vue"),
+      beforeEnter: guardRoute,
     },
-  ]
-})
+  ],
+});
 
-export default router
+router.beforeEach(async (to) => {
+  var userStore = useUserStore();
+  var loggedIn = userStore.isLoggedIn;
+
+  const publicPages = ["/", "/login"];
+  const authRequired = !publicPages.includes(to.path);
+
+  if (authRequired && loggedIn) {
+    console.log("you good");
+  }
+});
+
+export default router;
