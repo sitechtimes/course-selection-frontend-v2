be49e4814a4d98c44cee4efd998ce6c355b9c630
wrrius <2.dan.0.bay.0@gmail.com>
2023-07-23 17:35:43 -0400

    done!
    only gonna push when ivy says its fine

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@ import NavBar from './components/NavbarComponents/NavBar.vue';
 </script>
 
 <template>
+      <div class="fixed place-content-center items-center inset-x-0 bottom-0 w-full flex z-20 text-[#174616] bg-[#A8D480] h-1/4 lg:text-5xl md:text-3xl text-2xl duration-[0.75s] translate-y-full" id="notification">
+      <h1 class="">Your survey has been submitted</h1>
+    </div>
   <div id="app">
     <nav-bar></nav-bar>
     <router-view></router-view>
   </div>
+  
 </template>
 
 <style scoped>
