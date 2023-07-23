1dc1d6df877ae770ee62cdaa1d46b7f7b61b2ffd
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-07-23 12:29:35 -0400

    exit reminder

--- src/views/ExitReminderTest.vue
+++ src/views/ExitReminderTest.vue
@@ -1,70 +1,43 @@
 <template>
-  <div>hello</div>
+  <div id="app" v-cloak>
+  <form>
+    <p>
+    <label for="name">stuff</label>
+    <input type="text" id="name" v-model="stuff1">
+    </p>
+    <p>
+      <input type="submit" @click="removeHandler()">
+    </p>
+    <p>{{ stuff1 }}</p>
+  </form>
+</div>
 </template>
 
 <script lang="ts" setup>
-// var isDirty = function() { return false; }
-import { onUnmounted, onBeforeMount } from 'vue';
-import { onBeforeRouteLeave } from 'vue-router';
-const formSubmitting = false
+import { ref, watch } from 'vue';
+let stuff1 = ref('')
+let handler = null
 
-onBeforeRouteLeave(() => {
-//     if (confirmStayInDirtyForm()){
-//     // next(false)
-//     console.log('stay')
-//   } else {
-//     // Navigate to next view
-//     // next() 
-//     console.log('go')
-//   }
-confirmLeave()
-})
-
-
- function  confirmStayInDirtyForm() {
-    return !confirmLeave()
-  }
-
-  function beforeWindowUnload() {
-    if (confirmStayInDirtyForm()) {
-      // Cancel the event
-    //   e.preventDefault()
-      // Chrome requires returnValue to be set
-    //   e.returnValue = ''
-    }  
+function test() {
+  console.log(stuff1)
 }
 
-// console.log(window)
-// window.onbeforeunload = confirmLeave();
-// window.onbeforeunload = function () {
-//     return window.confirm('yoohoo')
-// }
-function confirmLeave() {
-     window.confirm('Do you really want to leave? you have unsaved changes!')
+watch(() => stuff1.value, (newResponse, oldResponse) => {
+  console.log('ok')
+  handler =  function (e) {
+    // Cancel the event
+    e.preventDefault(); 
+    // Chrome requires returnValue to be set
+    e.returnValue = '';
+  };
+  window.addEventListener('beforeunload', handler);
+  console.log('add watcher'); 
+})
+
+function removeHandler() {
+      if(handler) window.removeEventListener('beforeunload',handler);
+      return true;
   }
-    window.addEventListener("beforeunload", confirmLeave());
-    // window.addEventListener("beforeunload", beforeWindowUnload());
-    // window.addEventListener("beforeunload", function (e) {
-    //     // if (formSubmitting || !isDirty()) {
-    //     // if (formSubmitting) {
-    //     //     return undefined;
-    //     // }
-        
-    //     // var confirmationMessage = 'It looks like you have been editing something. '
-    //     //                         + 'If you leave before saving, your changes will be lost.';
-
-    //     // (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    //     // return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-    //     if (confirmStayInDirtyForm()) {
-    //   // Cancel the event
-    //   e.preventDefault()
-    //   // Chrome requires returnValue to be set
-    //   e.returnValue = ''
-    // } 
-    // });
-
-    
-
 </script>
 
 <style>
