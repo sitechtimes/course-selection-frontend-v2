5ee149a61a5bff3428a6dbace746447ec9eac1ba
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-07-24 09:37:35 -0400

    import

--- src/views/GuidanceCalendar.vue
+++ src/views/GuidanceCalendar.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="event h-screen w-full flex flex-row justify-center items-center">
+    <GuidanceCalendar />
+    <Upcoming />
     <createevent />
   </div>
 </template>
 
 <script setup lang="ts">
 import createevent from "../components/GuidanceComponents/CreateEvent.vue";
+import GuidanceCalendar from "../components/GuidanceComponents/GuidanceCalendar.vue";
 </script>
 
 <style scoped>
