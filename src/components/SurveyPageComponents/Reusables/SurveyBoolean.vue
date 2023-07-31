bdf0d436f72f13f4abb3250da33b80aa05d430b6
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-07-31 17:09:38 -0400

    Update SurveyBoolean.vue

--- src/components/SurveyPageComponents/Reusables/SurveyBoolean.vue
+++ src/components/SurveyPageComponents/Reusables/SurveyBoolean.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full flex items-start justify-center">
+  <div class="w-full h-full flex items-start sm:items-center justify-center">
     <fieldset ref="form" >
       <legend class="text-lg md:text-xl xl:text-3xl overflow-visible text-center sm:text-left">{{ question.question }}</legend>
       <div class="flex flex-row text-lg md:text-xl xl:text-2xl justify-center sm:justify-start">
