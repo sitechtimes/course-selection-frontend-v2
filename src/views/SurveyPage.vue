f88a7dc30dcee509abc658cae6aecc045c770a0a
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-08-01 09:28:28 -0400

    Update SurveyPage.vue

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -89,7 +89,9 @@ watch(() => studentStore.answeredSurvey[0], (newResponse, oldResponse) => {
 
 <template>
   <div class="h-[80vh] flex flex-col justify-center items-center space-y-8">
-    <p v-if="surveyStore.loading">Setting things up...</p>
+    <div v-if="surveyStore.loading" class="animate-spin">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
+    </div>
     <div
       v-else
       class="w-11/12 md:w-4/5 lg:w-3/4 flex flex-col items-center min-h-[20rem] h-5/6 overflow-auto"
