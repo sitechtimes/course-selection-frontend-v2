ededdd55114ee69a05eb5d312dbabbef49dc4463
bridgetf7 <fbridget0404@gmail.com>
2023-07-31 20:19:47 -0400

    fgdzs

--- src/components/SurveyPageComponents/Reusables/ScrollPage.vue
+++ src/components/SurveyPageComponents/Reusables/ScrollPage.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="arrows flex flex-col w-f50 h-50 bottom-2 right-5 fixed">
-    <div @click="scrollTop"
-      class="scrollTop none-topArrow bg-[#DEE9C8] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-50 p-4 mb-2.5 text-center flex flex-col align-center">
+  <div class="arrows flex flex-col h-50 bottom-2 right-5 fixed">
+    <div @click="scrollTop" v-if="ShowScrollTop" :class="{ 'pop-in': ShowScrollTop, 'pop-out': !ShowScrollTop }"
+      class="bg-[#DEE9C8] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-50 p-4 mb-2.5 text-center flex flex-col align-center">
       <p>go to top</p>
     </div>
-    <div @click="scrollBottom"
-      class="scrollBottom none-bottomArrow bg-[#DEE9C8] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-fit p-4 mb-2.5 flex flex-col align-center">
+    <div @click="scrollBottom" v-if="ShowScrollBottom"
+      :class="{ 'pop-in': ShowScrollBottom, 'pop-out': !ShowScrollBottom }"
+      class="bg-[#DEE9C8] shadow-[2px_3px_2px_rgba(0,0,0,0.25)] w-fit p-4 mb-2.5 flex flex-col align-center">
       <p>go to bottom</p>
     </div>
   </div>
 </template>
-  
+
+
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+
+const ShowScrollTop = ref(false);
+const ShowScrollBottom = ref(true);
 
 function scrollTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 function scrollBottom() {
@@ -23,32 +28,25 @@ function scrollBottom() {
 }
 
 function scrolledEnough() {
-  const scrollTop = document.querySelector(".scrollTop");
-  const scrollBottom = document.querySelector(".scrollBottom");
+  const windowHeight = window.innerHeight;
+  const oneThirdHeight = windowHeight;
+  const twoThirdsHeight = windowHeight * 2;
 
-  if (window.scrollY > 0) { 
-    scrollTop.style.display = "flex";
-    scrollTop.classList.remove('pop-out')
-    scrollTop.classList.add('pop-in')
-    scrollBottom.style.display = "flex";
-    scrollBottom.classList.remove('pop-out')
-    scrollBottom.classList.add('pop-in')
+  if (window.scrollY <= oneThirdHeight) {
+    ShowScrollTop.value = false;
+    ShowScrollBottom.value = true;
+  } else if (window.scrollY >= twoThirdsHeight) {
+    ShowScrollTop.value = true;
+    ShowScrollBottom.value = false;
   } else {
-    scrollTop.style.display = "none";
-    scrollTop.classList.add('pop-out')
-    scrollTop.classList.remove('pop-in')
-    scrollBottom.style.display = "none";
-    scrollBottom.classList.add('pop-out')
-    scrollBottom.classList.remove('pop-in')
+    ShowScrollTop.value = true;
+    ShowScrollBottom.value = true;
   }
-
 }
+
 onMounted(() => {
   window.addEventListener("scroll", scrolledEnough);
-  scrollBottom.style.display = "flex";
-    scrollBottom.classList.remove('pop-out')
-    scrollBottom.classList.add('pop-in')
-})
+});
 </script>
   
 <style scoped>
@@ -78,28 +76,9 @@ onMounted(() => {
     transform: translatey(0);
   }
 
-
   to {
     opacity: 0;
     transform: translatey(150px);
   }
-}
-
-.show-topArrow {
-  opacity: 1;
-}
-
-.none-topArrow {
-  opacity: 0;
-}
-
-.scrollTop,
-.scrollBottom {
-  z-index: 3;
-  display: none;
-  height: 3rem;
-  opacity: 1;
-  scroll-behavior: smooth;
-  cursor: pointer;
 }
 </style>
