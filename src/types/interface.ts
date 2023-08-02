2e30b05c109bba30a1f8db20f932432c2949d3b9
richie <113203265+R1chi333@users.noreply.github.com>
2023-08-02 08:13:58 -0400

    .

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -19,6 +19,7 @@ export interface guidanceData {
     allSurveys: allSurveys
     guidance: guidanceStudentData
     user: userData
+    currentlyViewing: studentsShown
 }
 
 // student store interface
