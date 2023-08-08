bc1822f3800b387b6588b5089ba4b405793bb925
IvyZ23 <ivy.zhu23@sitechhs.com>
2023-08-08 09:07:14 -0400

    Update user.ts

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -271,8 +271,8 @@ export const useUserStore = defineStore("user", {
           const expiration = date.setHours(date.getHours() + 1);
 
           this.expire_time = expiration
-
-          this.getUserType(); 
+  
+          this.getUserType(); //make dj rest auth return user type (backend) to remove this function
         });
     },
     async changeMeeting(email: string, newTime: string) {
