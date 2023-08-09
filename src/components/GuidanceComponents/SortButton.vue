d3ee466f930f35a76fc74eec23bb0fb6c790d28d
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2023-08-09 16:24:12 -0400

    deploy test

--- src/components/GuidanceComponents/SortButton.vue
+++ src/components/GuidanceComponents/SortButton.vue
@@ -164,7 +164,7 @@ const sortBy = (sort: {sortBy:string, text:string}) => {
   const sortFunction = eval(sort.sortBy)
   selected.value = sort.text
   isOpen.value = false
-  return (guidanceStore.currentlyViewing.sort(sortFunction))
+  return (guidanceStore.currentlyViewing.sort(com))
 }
 </script>
 
