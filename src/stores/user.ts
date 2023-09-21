3f1cc3f42f398c7d8d04a5f62ecdf81d660b3b36
vvwenv <90654380+vvwenv@users.noreply.github.com>
2023-09-21 10:42:30 -0400

    upd

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -127,12 +127,13 @@ export const useUserStore = defineStore("user", {
             }
           )
           .then((res) => {
-            const guidanceStore = useGuidanceStore()
-            guidanceStore.allAnsweredSurveys = res.data.data.allAnsweredSurveys
-            guidanceStore.allStudents = res.data.data.allStudents
-            guidanceStore.allSurveys = res.data.data.allSurveys
-            guidanceStore.guidance = res.data.data.guidance
-            guidanceStore.user = res.data.data.user
+            const guidanceStore = useGuidanceStore();
+            guidanceStore.allAnsweredSurveys = res.data.data.allAnsweredSurveys;
+            guidanceStore.allStudents = res.data.data.allStudents;
+            guidanceStore.allSurveys = res.data.data.allSurveys;
+            guidanceStore.guidance = res.data.data.guidance;
+            guidanceStore.user = res.data.data.user;
+            guidanceStore.surveyStats = res.data.data.surveyStats;
 
             this.loading = false;
           });
@@ -191,37 +192,46 @@ export const useUserStore = defineStore("user", {
             }
           )
           .then((res: any) => {
-            const studentStore = useStudentStore()
-            studentStore.answeredSurvey = res.data.data.answeredSurvey
-            studentStore.student = res.data.data.student
-            studentStore.survey = res.data.data.survey
-            studentStore.user = res.data.data.user
+            const studentStore = useStudentStore();
+            studentStore.answeredSurvey = res.data.data.answeredSurvey;
+            studentStore.student = res.data.data.student;
+            studentStore.survey = res.data.data.survey;
+            studentStore.user = res.data.data.user;
 
-            const surveyStore = useSurveyStore() 
-            const router = useRouter()
-            if(studentStore.student.homeroom === "") {
-              console.log("profile not updated")
+            const surveyStore = useSurveyStore();
+            const router = useRouter();
+            if (studentStore.student.homeroom === "") {
+              console.log("profile not updated");
             } else {
-              const currentDate = new Date()
-              const closeTime = studentStore.survey.dueDate.substring(0,10).split("-")
-              if(studentStore.answeredSurvey.length !== 0 && studentStore.answeredSurvey[0].status === 'FINALIZED'){
-                  surveyStore.open = false
+              const currentDate = new Date();
+              const closeTime = studentStore.survey.dueDate
+                .substring(0, 10)
+                .split("-");
+              if (
+                studentStore.answeredSurvey.length !== 0 &&
+                studentStore.answeredSurvey[0].status === "FINALIZED"
+              ) {
+                surveyStore.open = false;
               }
-              
+
               if (Number(closeTime[0]) < currentDate.getFullYear()) {
-                surveyStore.open = false
+                surveyStore.open = false;
               } else if (Number(closeTime[0]) === currentDate.getFullYear()) {
-                if (Number(closeTime[1]) < currentDate.getMonth() + 1) { // Get month starts at 0, not 1
-                  surveyStore.open = false
-                } else if (Number(closeTime[1]) === currentDate.getMonth() + 1) {
+                if (Number(closeTime[1]) < currentDate.getMonth() + 1) {
+                  // Get month starts at 0, not 1
+                  surveyStore.open = false;
+                } else if (
+                  Number(closeTime[1]) ===
+                  currentDate.getMonth() + 1
+                ) {
                   if (Number(closeTime[2]) < currentDate.getDate()) {
-                    surveyStore.open = false
+                    surveyStore.open = false;
                   }
                 }
               }
-              surveyStore.currentSurvey = studentStore.survey
-              }
-              this.loading = false;              
+              surveyStore.currentSurvey = studentStore.survey;
+            }
+            this.loading = false;
           });
       }
     },
@@ -265,16 +275,47 @@ export const useUserStore = defineStore("user", {
           this.email = response.data.user.email;
           this.first_name = response.data.user.first_name;
           this.last_name = response.data.user.last_name;
-          this.isLoggedIn = true;  
+          this.isLoggedIn = true;
 
-          const date = new Date()
+          const date = new Date();
           const expiration = date.setHours(date.getHours() + 1);
 
-          this.expire_time = expiration
-  
+          this.expire_time = expiration;
+
           this.getUserType(); //make dj rest auth return user type (backend) to remove this function
         });
     },
+    async EmailLogin(username: string, password: string) {
+      this.loading = true;
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_URL}/auth/login/`,
+          {
+            username: username,
+            password: password,
+          }
+        );
+
+        console.log(response);
+        this.access_token = response.data.access_token;
+        this.refresh_token = response.data.refresh_token;
+        this.email = response.data.user.email;
+        this.first_name = response.data.user.first_name;
+        this.last_name = response.data.user.last_name;
+        this.isLoggedIn = true;
+
+        const date = new Date();
+        const expiration = date.setHours(date.getHours() + 1);
+
+        this.expire_time = expiration;
+
+        this.getUserType(); // make dj rest auth return user type (backend) to remove this function
+      } catch (error) {
+        this.loading = false;
+        alert("Login failed. Please check your credentials.");
+      }
+    },
+
     async changeMeeting(email: string, newTime: string) {
       await axios
         .post(
@@ -296,17 +337,23 @@ export const useUserStore = defineStore("user", {
           }
         )
         .then((res) => {
-          console.log(res)
-          const guidanceStore = useGuidanceStore()
-          const studentIndexAll = guidanceStore.allStudents.edges.findIndex(student => student.node.user.email === email)
-          const studentIndex = guidanceStore.guidance.students.findIndex(student => student.user.email === email)
+          console.log(res);
+          const guidanceStore = useGuidanceStore();
+          const studentIndexAll = guidanceStore.allStudents.edges.findIndex(
+            (student) => student.node.user.email === email
+          );
+          const studentIndex = guidanceStore.guidance.students.findIndex(
+            (student) => student.user.email === email
+          );
 
-          if(studentIndex > -1) {
-            guidanceStore.guidance.students[studentIndex].meeting = res.data.data.updateMeeting.student.meeting
+          if (studentIndex > -1) {
+            guidanceStore.guidance.students[studentIndex].meeting =
+              res.data.data.updateMeeting.student.meeting;
           }
 
-          console.log(guidanceStore.allStudents.edges[studentIndexAll].node)
-          guidanceStore.allStudents.edges[studentIndexAll].node.meeting = res.data.data.updateMeeting.student.meeting
+          console.log(guidanceStore.allStudents.edges[studentIndexAll].node);
+          guidanceStore.allStudents.edges[studentIndexAll].node.meeting =
+            res.data.data.updateMeeting.student.meeting;
         });
     },
     async addFlag(email: string, newFlag: string) {
@@ -330,15 +377,21 @@ export const useUserStore = defineStore("user", {
           }
         )
         .then((res) => {
-          const guidanceStore = useGuidanceStore()
-          const studentIndexAll = guidanceStore.allStudents.edges.findIndex(student => student.node.user.email === email)
-          const studentIndex = guidanceStore.guidance.students.findIndex(student => student.user.email === email)
+          const guidanceStore = useGuidanceStore();
+          const studentIndexAll = guidanceStore.allStudents.edges.findIndex(
+            (student) => student.node.user.email === email
+          );
+          const studentIndex = guidanceStore.guidance.students.findIndex(
+            (student) => student.user.email === email
+          );
 
-          if(studentIndex > -1) {
-            guidanceStore.guidance.students[studentIndex].flag = res.data.data.updateFlag.student.flag
+          if (studentIndex > -1) {
+            guidanceStore.guidance.students[studentIndex].flag =
+              res.data.data.updateFlag.student.flag;
           }
 
-          guidanceStore.allStudents.edges[studentIndexAll].node.flag = res.data.data.updateFlag.student.flag
+          guidanceStore.allStudents.edges[studentIndexAll].node.flag =
+            res.data.data.updateFlag.student.flag;
         });
     },
   },
