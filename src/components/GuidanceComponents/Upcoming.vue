1985f673a621cf53249f91a77f4606858ade6a66
bridgetf7 <fbridget0404@gmail.com>
2023-09-27 20:33:53 -0400

    put the meetings into upcoming events

--- src/components/GuidanceComponents/Upcoming.vue
+++ src/components/GuidanceComponents/Upcoming.vue
@@ -1,43 +1,62 @@
 <template>
-  <div class="upcoming">
-    <div class="title border-gray-500">
-      <h1 class="py-2 px-4 text-center font-extrabold">Upcoming Events</h1>
+  <div class="w-[25vw] border border-gray-500 rounded-md">
+    <div class="border-b border-gray-500">
+      <h1 class="py-2 px-4 text-center font-extrabold text-2xl">Upcoming Events</h1>
     </div>
-    <div class="events">
-      <ul class="eventContainer">
-        <h2 class="date">Nov 8, 2023</h2>
-        <li class="event">Jessie's Birthday</li>
-      </ul>
-      <ul class="eventContainer">
-        <h2 class="date">Nov 8, 2023</h2>
-        <li class="event">Jessie's Birthday</li>
-      </ul>
+    <div class="p-4">
+      <div class="overflow-y-auto max-h-100">
+        <ul class="my-4" v-for="(meeting, index) in studentInfo" :key="index">
+          <h2 class="font-bold text-lg">{{ formatDate(meeting.meetingDate) }}</h2>
+          <li class="ml-6 list-disc">{{ meeting.name }}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
-<style scoped>
-.upcoming {
-  width: 35%;
-  border: solid grey 2px;
-  border-radius: 0.5rem;
+
+<script setup lang="ts">
+import { ref, Ref, onMounted } from "vue";
+import { useGuidanceStore } from "../../stores/guidance";
+import { StudentMeeting } from "../../types/interface";
+
+const guidanceStore = useGuidanceStore();
+
+const studentInfo: Ref<StudentMeeting[]> = ref([]); //initalise array as an array
+
+
+//filter out valid meetings within the guidance store
+const validMeetings = guidanceStore.allStudents.edges.filter(
+  (student) =>
+    student.node.meeting !== null && student.node.meeting !== undefined
+);
+
+const currentDate = new Date();
+for (const student of validMeetings) { //for each student with a valid meeting...
+  const meetingDate = new Date(student.node.meeting);
+  //if the meeting is before the current date, dont show it
+  if (meetingDate > currentDate) {
+    const studentMeetings = {
+      name: `${student.node.user.firstName} ${student.node.user.lastName}`,//extract first and last name
+      meetingDate: student.node.meeting,
+    };
+    studentInfo.value.push(studentMeetings); //push the meetings into the array
+  }
 }
-.title {
-  font-size: 1.5rem;
-  border-bottom: grey solid 2px;
+
+//sort the meetings
+studentInfo.value.sort((a, b) => {
+  return new Date(a.meetingDate).getTime() - new Date(b.meetingDate).getTime();
+});
+
+//convert the date to weekday, mm/dd/yy
+function formatDate(meetingDate: string): string {
+  const event = new Date(meetingDate);
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  return event.toLocaleDateString("en-US", options);
 }
-.events {
-  margin: 1rem 1.5rem 0rem 1.5rem;
-}
-.eventContainer {
-  margin-top: 1rem;
-}
-.date {
-  font-weight: bold;
-  font-size: 1.2rem;
-}
-.event {
-  margin-left: 1rem;
-  list-style-type: disc;
-  list-style-position: inside;
-}
-</style>
+</script>
