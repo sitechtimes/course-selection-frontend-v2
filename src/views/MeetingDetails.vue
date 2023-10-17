ee8fd685cd669750e7b727edce9bab329fcda086
cassidyrobles <cassidyrobles19@gmail.com>
2023-10-17 10:59:24 -0400

    drhrdy

---
+++ src/views/MeetingDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    <MeetingDetails />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MeetingDetails from "../components/GuidanceComponents/MeetingDetails.vue";
+
+</script>
+
