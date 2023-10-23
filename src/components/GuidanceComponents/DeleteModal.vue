fe0b4da393dbb4918c90f8915c8616ff7b7f7a9e
bridgetf7 <fbridget0404@gmail.com>
2023-10-23 14:30:25 -0400

    Update DeleteModal.vue

--- src/components/GuidanceComponents/DeleteModal.vue
+++ src/components/GuidanceComponents/DeleteModal.vue
@@ -1,8 +1,13 @@
 <template>
     <div
-        class="absolute left-[20%] right-[20%] top-[30%] bg-white flex flex-row m-auto mt-5 w-fit h-fit rounded-[1rem] border border-black">
-        <h2>Are you sure?</h2>
+        class="absolute left-[20%] right-[20%] top-[30%] bg-white flex flex-col m-auto mt-5 p-4 w-fit h-fit rounded-[1rem] border border-black">
+        <h2 class="h2 font-bold text-[1.5rem] m-8 mb-4">Are you sure?</h2>
         <p>Delete name's meeting on date</p>
+        <div class="flex items-center justify-center align-middle"> <button type="submit"
+            class="mb-8 duration-300 font-bold px-4 py-2 rounded-2xl w-fit h-fit opacity-0.5:hover">Cancel</button>
+        <button type="submit"
+            class="duration-300 font-bold bg-[#f28e8e] px-4 py-2 rounded-2xl w-fit h-fit opacity-0.5:hover">Delete</button></div>
+       
     </div>
 </template>
   
