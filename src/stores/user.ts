eb2d77c32337411eb0263d25833d9fd717c4173b
KevinZheng29 <zhengkevin29@gmail.com>
2023-10-24 17:14:10 -0400

    10/24
    refresh

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,6 +5,10 @@ import { useGuidanceStore } from "./guidance";
 import { useRouter } from "vue-router";
 import axios from "axios";
 import { user, account_type, userData } from "../types/interface";
+import { sharedState } from "./function";
+const callFunctionInChild2 = () => {
+  sharedState.sharedFunction.value(); // Call the function in Child 2
+};
 
 export const useUserStore = defineStore("user", {
   state: (): user => ({
@@ -367,6 +371,7 @@ export const useUserStore = defineStore("user", {
           guidanceStore.allStudents.edges[studentIndexAll].node.meeting =
             res.data.data.updateMeeting.student.meeting;
           guidanceStore.allStudents.edges[studentIndexAll].node.description = description;
+          callFunctionInChild2()
         });
     },
     async addFlag(email: string, newFlag: string) {
