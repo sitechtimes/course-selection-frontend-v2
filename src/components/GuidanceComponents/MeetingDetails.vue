9c5e9a57ff03c5872d41ab80af03c77fe3b8fcb7
cassidyrobles <cassidyrobles19@gmail.com>
2023-10-25 11:02:15 -0400

    finish pls

--- src/components/GuidanceComponents/MeetingDetails.vue
+++ src/components/GuidanceComponents/MeetingDetails.vue
@@ -14,7 +14,7 @@
       </div>
       <!-- details -->
       <h3 class="formt flex flex-row text-[#717494] ml-8 xl:text-2xl font-bold" for="emails">
-        <svg class="w-px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <svg class="w-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <path
             d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
         </svg>
