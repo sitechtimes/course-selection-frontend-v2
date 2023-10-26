c94801ec62426ea561445d3451d4ef75932b3706
cassidyrobles <cassidyrobles19@gmail.com>
2023-10-26 10:28:50 -0400

    ,khjg

--- src/components/GuidanceComponents/MeetingDetails.vue
+++ src/components/GuidanceComponents/MeetingDetails.vue
@@ -20,7 +20,7 @@
         </svg>
         Student
       </h3>
-      <p class="ml-8">{{ meetingDetails.name }}</p>
+      <p class="ml-8 p-3">{{ meetingDetails.name }}</p>
   <div class="times flex flex-col lg:flex-row">
           <div>
             <h3 class="formt flex flex-row text-[#717494] ml-8 xl:text-2xl font-bold" for="date">
@@ -30,7 +30,7 @@
                   d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192H400V448c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192z" />
               </svg>
               Date</h3>
-              <p class="ml-8">{{ meetingDetails.date }}</p>
+              <p class="ml-8 p-3">{{ meetingDetails.date }}</p>
           </div>
           <div>
             <h3 class="formt flex flex-row text-[#717494] ml-8 xl:text-2xl font-bold" for="time">
@@ -40,7 +40,7 @@
                   d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
               </svg>
               Time</h3>
-              <p class="ml-8">{{ meetingDetails.time }}</p>
+              <p class="ml-8 p-3">{{ meetingDetails.time }}</p>
           </div>
     </div>
 
