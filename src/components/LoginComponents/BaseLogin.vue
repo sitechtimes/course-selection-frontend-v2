f10794e7663b01a81a23f01f2cb5b90b23e4422a
cassidyrobles <cassidyrobles19@gmail.com>
2023-10-30 11:02:34 -0400

    finished

--- src/components/LoginComponents/BaseLogin.vue
+++ src/components/LoginComponents/BaseLogin.vue
@@ -19,12 +19,12 @@ const password = ref("")
         <h1 class="text-4xl mb-4 font-bold">Login</h1>
         <div class="flex flex-col w-11/12 max-w-[21rem] space-y-5 rounded-md">
             <LoginInput id="email" label="email" name="email" type="email" required :value="username"
-                @update="newValue => username = newValue">Username</LoginInput>
+                @update ="newValue => username = newValue">Username</LoginInput>
             <LoginInput id="password" label="password" name="password" type="password" required :value="password"
                 @update="newValue => password = newValue">Password</LoginInput>
         </div>
-        <button class="bg-zinc-300 w-32 h-12 mt-4 rounded-md text-lg font-semibold hover:bg-zinc-200"
-            @click="userStore.EmailLogin(username, password)">
+        <button id="button" class="bg-zinc-300 w-32 h-12 mt-4 rounded-md text-lg font-semibold hover:bg-zinc-200"
+            @keyup.enter="userStore.EmailLogin(username, password)">
             Login
         </button>
         <GoogleLogin :callback="userStore.GoogleLogin" popup-type="TOKEN">
@@ -34,4 +34,6 @@ const password = ref("")
                 <p class="font-bold text-lg">Sign in with Google</p>
             </button>
         </GoogleLogin>
+    
     </div></template>
+  
