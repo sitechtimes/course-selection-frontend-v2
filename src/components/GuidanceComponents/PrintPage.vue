08b4d1eacf1a4c19694c384023799f00809d87ab
bridgetf7 <fbridget0404@gmail.com>
2023-11-14 10:06:02 -0500

    Update PrintPage.vue

--- src/components/GuidanceComponents/PrintPage.vue
+++ src/components/GuidanceComponents/PrintPage.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="flex flex-col">
-    <div
-      id="printPage"
-      class="flex w-[40vw] m-4 border border-gray-500 rounded-md"
-    >
+    <div id="printPage" class="flex w-[40vw] m-4 p-2 border border-gray-500 rounded-md">
       <div class="p-4">
         <div class="overflow-y-auto max-h-100">
           <ul class="my-4">
@@ -13,21 +10,16 @@
               {{ meetingTime }} on {{ meetingDate }}. Please meet with them
               during the specified time. Your guidance counselor has made the
               following notes:
-              <br />
-              {{ meetingDescription }}
             </p>
+            <br />
+            <p>{{ meetingDescription }}</p>
           </ul>
         </div>
       </div>
     </div>
-    <div
-      class="item submit ml-6 mb-6 xl:text-2xl transition duration-300 hover:opacity-50 cursor-pointer w-fit"
-    >
-      <button
-        class="flex flex-row items-center font-bold text-[1.2rem] bg-[#e5e7be] px-4 py-2 rounded-lg w-fit h-fit"
-        type="submit"
-        @click="printMeetingTicket"
-      >
+    <div class="item submit ml-4 mb-6 xl:text-2xl transition duration-300 hover:opacity-50 cursor-pointer w-fit">
+      <button class="flex flex-row items-center font-bold text-[1.2rem] bg-[#e5e7be] px-4 py-2 rounded-lg w-fit h-fit"
+        type="submit" @click="printMeetingTicket">
         <PrinterIcon class="mr-3" /> Print
       </button>
     </div>
@@ -42,6 +34,7 @@ import PrinterIcon from "../icons/PrinterIcon.vue";
 import { useRoute } from "vue-router";
 //@ts-ignore
 import dateformat from "dateformat";
+
 const route = useRoute();
 const email = route.params.email;
 
@@ -65,14 +58,19 @@ const DateAndTime = guidanceStore.allStudents.edges[studentIndex].node.meeting;
 
 const meetingTime = dateformat(DateAndTime, "shortTime");
 
-const meetingDate = new Date(DateAndTime as string);
+const meetingDate = dateformat(DateAndTime, "shortDate");
 
 const meetingDescription =
   guidanceStore.allStudents.edges[studentIndex].node.description;
 
-function printMeetingTicket() {
+const printMeetingTicket = () => {
+  const allBody = document.body.innerHTML;
+  const partPrint = document.getElementById("printPage").innerHTML;
+  document.body.innerHTML = partPrint;
   window.print();
-}
+  document.body.innerHTML = allBody;
+};
+
 /*
 defineProps({
   meetingDetails: {
