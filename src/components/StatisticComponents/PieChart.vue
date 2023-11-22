df7875c00c49edf2f45bc5ffc76d23826bc0cfd8
KevinZheng29 <zhengkevin29@gmail.com>
2023-11-22 10:32:38 -0500

    Update PieChart.vue

--- src/components/StatisticComponents/PieChart.vue
+++ src/components/StatisticComponents/PieChart.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="flex flex-col justify-center align-center items-center">
-    <div class="text-2xl md:text-3xl font-semibold sm:flex text-center">Course Rankings</div>
+    <div class="text-2xl md:text-3xl font-semibold sm:flex text-center">
+      Course Rankings
+    </div>
     <!-- drop-down menu for years-->
-    <select v-model="selectedYear" class="space rounded-md border border-solid border-zinc-400 h-10 p-2 mt-2 w-80">
+    <select
+      v-model="selectedYear"
+      class="space rounded-md border border-solid border-zinc-400 h-10 p-2 mt-2 w-80"
+    >
       <option v-for="year in years" :value="year" :key="year">
         {{ year }}
       </option>
@@ -11,8 +16,13 @@
       <p>Please select a year from the list above</p>
     </div>
     <!-- drop-down menu for courses-->
-    <select v-model="selectedCourse" class="space rounded-md border border-solid border-zinc-400 h-10 p-2 mt-2 w-80">
-      <option v-for="course in courses" :key="course" :value="course">{{ course }}</option>
+    <select
+      v-model="selectedCourse"
+      class="space rounded-md border border-solid border-zinc-400 h-10 p-2 mt-2 w-80"
+    >
+      <option v-for="course in courses" :key="course" :value="course">
+        {{ course }}
+      </option>
     </select>
     <div class="w-[70rem] mt-2" v-if="loaded && selectedCourse && selectedYear">
       <Pie :options="chartOptions" :data="getChartData" />
@@ -24,85 +34,85 @@
 </template>
 
 <script lang="ts" setup>
-import { Pie } from 'vue-chartjs';
-import { useGuidanceStore } from '../../stores/guidance';
-import { ref, Ref, onMounted, computed } from 'vue';
-import { useUserStore } from '../../stores/user';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import axios from 'axios';
+import { Pie } from "vue-chartjs";
+import { useGuidanceStore } from "../../stores/guidance";
+import { ref, Ref, onMounted, computed } from "vue";
+import { useUserStore } from "../../stores/user";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import axios from "axios";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const loaded = ref(true);
 const guidanceStore = useGuidanceStore();
 const userStore = useUserStore();
-const selectedCourse = ref('');
-const selectedYear = ref('');
-const storedYears = guidanceStore.surveyStats.edges
-let years = storedYears.map((yearSelected)=> yearSelected.node.year);
+const selectedCourse = ref("");
+const selectedYear = ref("");
+const storedYears = guidanceStore.surveyStats.edges;
+let years = storedYears.map((yearSelected) => yearSelected.node.year);
 
 onMounted(async () => {
   const access_token = userStore.access_token;
-  const baseURL = `${import.meta.env.VITE_URL}/guidance/stats`; 
+  const baseURL = `${import.meta.env.VITE_URL}/guidance/stats`;
 
   try {
     const response = await axios.get(baseURL, {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${access_token}`,
       },
     });
-
-    stats.value = JSON.parse(response.data); 
     loaded.value = true;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 });
 
 // if a new year is selected from the dropdown, find the index where the stats are located and parse it into the stats
-const stats = computed(()=>{ 
-  if(selectedYear.value !== ''){
+const stats = computed(() => {
+  if (selectedYear.value !== "") {
     //@ts-ignore
-    const indexSelectedYear = years.indexOf(selectedYear.value)
-    return JSON.parse(guidanceStore.surveyStats.edges[indexSelectedYear].node.stats);
-}
-})
+    const indexSelectedYear = years.indexOf(selectedYear.value);
+    return JSON.parse(
+      guidanceStore.surveyStats.edges[indexSelectedYear].node.stats
+    );
+  }
+});
 
- //returns each course name
+//returns each course name
 const courses = computed(() => {
   //check if stats is defined before using Object.keys()
   return stats.value ? Object.keys(stats.value) : [];
- 
 });
- 
+
 const chartOptions = ref({
   responsive: true,
 });
 
-const getChartData = computed(() => { //use computed properties to recalculate on render
+const getChartData = computed(() => {
+  //use computed properties to recalculate on render
   const chartData: {
-  labels: string[];
-  datasets: {
-    data: number[];
-    backgroundColor: string[];
-    label: string;
-  }[];
-} = {
-  labels: [],
-  datasets: [
-    {
-      data: [],
-      backgroundColor: ['#C5D4A4'],
-      label: '# of students',
-    },
-  ],
-};
-
+    labels: string[];
+    datasets: {
+      data: number[];
+      backgroundColor: string[];
+      label: string;
+    }[];
+  } = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: ["#C5D4A4"],
+        label: "# of students",
+      },
+    ],
+  };
 
   for (let i = 0; i < 40; i++) {
-    const randomColours = "#" + Math.floor(Math.random() * 16777215).toString(16); //toString(16) turns it into a hexadecimal
-    chartData.datasets[0].backgroundColor.push(randomColours)
+    const randomColours =
+      "#" + Math.floor(Math.random() * 16777215).toString(16); //toString(16) turns it into a hexadecimal
+    chartData.datasets[0].backgroundColor.push(randomColours);
   }
 
   if (selectedCourse.value) {
@@ -114,7 +124,7 @@ const getChartData = computed(() => { //use computed properties to recalculate o
       chartData.datasets[0].data.push(ranks[i]);
     }
   }
-  loaded.value = true
-  return chartData
+  loaded.value = true;
+  return chartData;
 });
 </script>
