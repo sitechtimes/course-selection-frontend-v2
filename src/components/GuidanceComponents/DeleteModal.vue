84bf6a3c8c1be7e3c2bee5624d6190e58f2b5762
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-12-19 11:01:23 -0500

    ?

--- src/components/GuidanceComponents/DeleteModal.vue
+++ src/components/GuidanceComponents/DeleteModal.vue
@@ -1,18 +1,29 @@
 <template>
-    <div v-if="showDetails" 
-        class="absolute left-[20%] right-[20%] top-[40%] bg-white flex flex-col m-auto mt-5 p-8 w-fit h-fit rounded-[1rem] border border-black">
-        <h2 class="h2 font-bold text-[1.5rem] mb-2 text-left">Are you sure?</h2>
-        <p class="my-4">Delete {{ meetingDetails.name }}'s meeting on {{ meetingDetails.date }}</p>
-        <p class="mb-6">{{ meetingDetails.name }} will be notified of this change</p>
-        <div class="flex flex-row items-center justify-end"> <button @click="toggleDetails"
-                class="duration-300 font-bold px-4 py-2 rounded-2xl w-fit h-fit opacity-0.5:hover">Cancel</button>
-            <button @click="deleteMeeting"
-                class="duration-300 font-bold bg-[#f28e8e] px-4 py-2 rounded-lg w-fit h-fit opacity-0.5:hover">Delete</button>
-        </div>
-
+  <div
+    v-if="showDetails"
+    class="absolute left-[20%] right-[20%] top-[40%] bg-white flex flex-col m-auto mt-5 p-8 w-fit h-fit rounded-[1rem] border border-black">
+    <h2 class="h2 font-bold text-[1.5rem] mb-2 text-left">Are you sure?</h2>
+    <p class="my-4">
+      Delete {{ meetingDetails.name }}'s meeting on {{ meetingDetails.date }}
+    </p>
+    <p class="mb-6">
+      {{ meetingDetails.name }} will be notified of this change
+    </p>
+    <div class="flex flex-row items-center justify-end">
+      <button
+        @click="toggleDetails"
+        class="duration-300 font-bold px-4 py-2 rounded-2xl w-fit h-fit opacity-0.5:hover">
+        Cancel
+      </button>
+      <button
+        @click="deleteMeeting"
+        class="duration-300 font-bold bg-[#f28e8e] px-4 py-2 rounded-lg w-fit h-fit opacity-0.5:hover">
+        Delete
+      </button>
     </div>
+  </div>
 </template>
-  
+
 <script setup lang="ts">
 import { ref, Ref, defineProps } from "vue"
 
@@ -28,9 +39,14 @@ const toggleDetails = () => {
     showDetails.value = !showDetails.value
 }
 
-// const deleteMeeting = (this) => {
-//   this.parentNode.removeParent(Node);
-// }
+async function deletingMeetingInfo() {
+  const { access_token } = useUserStore();
+  try { 
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${access_token}`,
+    };
+    const deleteMeeting = await fetch(`${}`)
+  }
+}
 </script>
-
-  
