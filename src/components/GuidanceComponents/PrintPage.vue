919f341f861208cd402d461446a2ce7bf2ec2d54
KevinZheng29 <zhengkevin29@gmail.com>
2024-02-02 10:22:35 -0500

    Update PrintPage.vue

--- src/components/GuidanceComponents/PrintPage.vue
+++ src/components/GuidanceComponents/PrintPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col">
-    <div id="printPage" class="flex w-[40vw] m-4 p-2 border border-gray-500 rounded-md">
+    <div
+      id="printPage"
+      class="flex w-[40vw] m-4 p-2 border border-gray-500 rounded-md"
+    >
       <div class="p-4">
         <div class="overflow-y-auto max-h-100">
           <ul class="my-4">
@@ -21,9 +24,14 @@
         </div>
       </div>
     </div>
-    <div class="item submit ml-4 mb-6 xl:text-2xl transition duration-300 hover:opacity-50 cursor-pointer w-fit">
-      <button class="flex flex-row items-center font-bold text-[1.2rem] bg-[#e5e7be] px-4 py-2 rounded-lg w-fit h-fit"
-        type="submit" @click="printMeetingTicket">
+    <div
+      class="item submit ml-4 mb-6 xl:text-2xl transition duration-300 hover:opacity-50 cursor-pointer w-fit"
+    >
+      <button
+        class="flex flex-row items-center font-bold text-[1.2rem] bg-[#e5e7be] px-4 py-2 rounded-lg w-fit h-fit"
+        type="submit"
+        @click="printMeetingTicket"
+      >
         <PrinterIcon class="mr-3" /> Print
       </button>
     </div>
@@ -52,20 +60,24 @@ async function fetchStudentInfo() {
   const { access_token } = useUserStore();
   try {
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${access_token}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${access_token}`,
     };
     //GET request for meetings
-    const meetingsResponse = await fetch(`${import.meta.env.VITE_URL}/guidance/meetings`, {
-      method: 'GET',
-      headers: headers,
-    });
-    const meetingsData = (await meetingsResponse.json()).map(student => ({
-      name: student.name.split(',')
-        .map(part => part.trim().toLowerCase())
-        .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+    const meetingsResponse = await fetch(
+      `${import.meta.env.VITE_URL}/guidance/meetings`,
+      {
+        method: "GET",
+        headers: headers,
+      }
+    );
+    const meetingsData = (await meetingsResponse.json()).map((student) => ({
+      name: student.name
+        .split(",")
+        .map((part) => part.trim().toLowerCase())
+        .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
         .reverse()
-        .join(' '),
+        .join(" "),
       meetingDate: student.meeting,
       description: student.meeting_description,
       grade: student.grade,
@@ -73,7 +85,7 @@ async function fetchStudentInfo() {
     }));
 
     //find the index of the student with the specified email
-    const index = meetingsData.findIndex(student => student.email === email);
+    const index = meetingsData.findIndex((student) => student.email === email);
     studentIndex.value = index;
     if (index > -1) {
       studentName.value = meetingsData[index].name;
@@ -84,14 +96,14 @@ async function fetchStudentInfo() {
     }
     return meetingsData;
   } catch (error) {
-    console.log('Error:', error);
+    console.log("Error:", error);
   }
 }
 
 onMounted(async () => {
   const meetingsData = await fetchStudentInfo();
   if (studentIndex.value < 0) {
-    console.log('Student not found');
+    console.log("Student not found");
   }
 });
 
@@ -111,9 +123,8 @@ const printMeetingTicket = () => {
 
   const newWindow = window.open("", "", "width=800,height=900");
   newWindow.document.write(printPage);
-  window.print();
+  newWindow.print();
 };
-
 </script>
 
 <style scoped>
