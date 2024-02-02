457a9e098be354e59946c8bf54fc8ed76f5ecac6
KevinZheng29 <zhengkevin29@gmail.com>
2024-02-02 10:59:58 -0500

    Update GuidanceStudentList.vue

--- src/views/GuidanceStudentList.vue
+++ src/views/GuidanceStudentList.vue
@@ -2,28 +2,50 @@
   <div class="h-auto w-full flex flex-col justify-center items-center mb-10">
     <div class="flex flex-row items-center justify-center w-5/6">
       <div class="w-1/3 flex flex-row justify-evenly">
-        <div v-if="loading">
-          Loading students...
-        </div>
-        <div @click="viewAll = !viewAll"
-          class="h-10 px-4 w-60 mx-10 flex flex-row bg-primary-g text-black justify-evenly  font-semibold items-center cursor-pointer shadow-[4px_3px_3px_rgba(0,0,0,0.25)] ">
+        <div v-if="loading">Loading students...</div>
+        <div
+          @click="viewAll = !viewAll"
+          class="h-10 px-4 w-60 mx-10 flex flex-row bg-primary-g text-black justify-evenly font-semibold items-center cursor-pointer shadow-[4px_3px_3px_rgba(0,0,0,0.25)]"
+        >
           <label class="cursor-pointer">View all students</label>
           <input class="ml-2" type="checkbox" v-model="viewAll" />
         </div>
         <Sort class="mr-0" />
       </div>
-      <SearchBar class="w-2/3" type="text" v-model="input" placeholder="Search Students..." />
+      <SearchBar
+        class="w-2/3"
+        type="text"
+        v-model="input"
+        placeholder="Search Students..."
+      />
     </div>
     <StudentTable :newstudents="newStudents.slice(x, y)" />
-    <div class="max-w-[80%] overflow-x-auto mt-4 flex flex-row justify-between ">
-      <button class=" mx-2  bg-[#ebebeb] h-8 w-8 rounded-lg font-bold" @click="subtract" :disabled="currentPage === 1">
+    <div class="max-w-[80%] overflow-x-auto mt-4 flex flex-row justify-between">
+      <button
+        class="mx-2 bg-[#ebebeb] h-8 w-8 rounded-lg font-bold"
+        @click="subtract"
+        :disabled="currentPage === 1"
+      >
         ❮
       </button>
-      <button v-for="n in pages" @click="updatePage(n)"
-        :class="{ 'bg-[#cdeeb4] focus:bg-[#cdeeb4]': currentPage === n, 'bg-[#ebebeb]': currentPage !== n }"
-        class=" h-8 w-8 rounded-lg hover:opacity-75 ease-in-out duration-300 font-bold mx-2"> {{ n }} </button>
-      <button class=" mx-2  bg-[#ebebeb] h-8 w-8 rounded-lg font-bold" :disabled="currentPage === pages"
-        @click="add">❯</button>
+      <button
+        v-for="n in pages"
+        @click="updatePage(n)"
+        :class="{
+          'bg-[#cdeeb4] focus:bg-[#cdeeb4]': currentPage === n,
+          'bg-[#ebebeb]': currentPage !== n,
+        }"
+        class="h-8 w-8 rounded-lg hover:opacity-75 ease-in-out duration-300 font-bold mx-2"
+      >
+        {{ n }}
+      </button>
+      <button
+        class="mx-2 bg-[#ebebeb] h-8 w-8 rounded-lg font-bold"
+        :disabled="currentPage === pages"
+        @click="add"
+      >
+        ❯
+      </button>
     </div>
     <h5 class="mt-4">
       Page
@@ -35,12 +57,12 @@
 </template>
 
 <script setup lang="ts">
-import SearchBar from '../components/GuidanceComponents/SearchBar.vue';
-import Sort from '../components/GuidanceComponents/SortButton.vue';
-import StudentTable from '../components/GuidanceComponents/StudentTable.vue'
-import { useUserStore } from '../stores/user';
-import { studentGuidance } from '../types/interface'
-import { ref, Ref, computed, watch, onMounted } from 'vue'
+import SearchBar from "../components/GuidanceComponents/SearchBar.vue";
+import Sort from "../components/GuidanceComponents/SortButton.vue";
+import StudentTable from "../components/GuidanceComponents/StudentTable.vue";
+import { useUserStore } from "../stores/user";
+import { studentGuidance } from "../types/interface";
+import { ref, Ref, computed, watch, onMounted } from "vue";
 
 document.title = "Student List | SITHS Course Selection";
 
@@ -50,7 +72,7 @@ const loading = ref(false);
 
 async function fetchStudents() {
   const { access_token } = useUserStore();
-  loading.value = true; 
+  loading.value = true;
   try {
     // GET request for all students
     const profilesResponse = await fetch(
@@ -63,7 +85,7 @@ async function fetchStudents() {
         },
       }
     );
-    loading.value=false
+    loading.value = false;
     const data = await profilesResponse.json();
     allStudents.value = data;
   } catch (error) {
@@ -83,14 +105,13 @@ const newStudents = computed(() => {
   viewAll.value;
   return userStore.currentlyViewingStudents.filter(
     (student: studentGuidance) =>
-      (student.name)
-        .toLowerCase()
-        .indexOf(input.value.toLowerCase()) != -1 ||
+      student.name.toLowerCase().indexOf(input.value.toLowerCase()) != -1 ||
       student.email.indexOf(input.value) != -1
   );
 });
 
 const pages = computed(() => {
+  //problem here
   return Math.ceil(userStore.currentlyViewingStudents.length / pageCapacity);
 });
 
@@ -114,7 +135,7 @@ const updatePage = (pageNumber: number) => {
 
 onMounted(async () => {
   userStore.currentlyViewingStudents = userStore.guidanceStudents;
-})
+});
 
 watch(
   () => viewAll.value,
@@ -129,5 +150,4 @@ watch(
     updatePage(1);
   }
 );
-
 </script>
