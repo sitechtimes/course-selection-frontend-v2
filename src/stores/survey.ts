af6538f37cb6227616099c5f79ee186f5e8e1643
Henry Zheng <zhenghenry2@gmail.com>
2024-02-28 13:59:40 -0500

    add survey store functions

--- src/stores/survey.ts
+++ src/stores/survey.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { useUserStore } from "./user";
 import { useRouter } from "vue-router";
-import { grade, status, surveyAnswer, surveyQuestion, surveyStore, surveyStringAnswer, classReferenced } from "../types/interface";
+import { grade, status, surveyAnswer, surveyQuestion, surveyStore, surveyStringAnswer, classReferenced, studentSurveyData } from "../types/interface";
 import axios from "axios";
 
 export const useSurveyStore = defineStore("survey", {
@@ -13,6 +13,7 @@ export const useSurveyStore = defineStore("survey", {
     open: true,
     submit: false,
     missingAnswers: [],
+    studentCourses: {coursesTaken: [], coursesAvailable: []},
   }),
   getters: {
     //
@@ -44,9 +45,60 @@ export const useSurveyStore = defineStore("survey", {
         }
       })
     },
-    async setSurvey() {
+    async fetchSurvey() {
+      const userStore = useUserStore();
+      const url = userStore.userType === "student" ? "/student/survey/" : `/guidance/survey/${this.currentAnsweredSurvey.email}`;
+
+      const res = await fetch(import.meta.env.VITE_URL + url, {
+        headers: {
+          Authorization: `Bearer ${userStore.access_token}`,
+        },
+      });
+      const surveyData: studentSurveyData = await res.json();
+      console.log(surveyData);
+
+      this.currentSurvey = surveyData.survey;
+      this.currentAnsweredSurvey = surveyData.answeredSurvey;
+
+      this.studentCourses.coursesAvailable = surveyData.coursesAvailable;
+      this.studentCourses.coursesTaken = surveyData.coursesTaken;
+
+      this.currentResponse = JSON.parse(surveyData.answeredSurvey.answers);
+      console.log("Fetched and set student survey data.");
+    },
+    async postSurvey(status: "COMPLETE" | "FINALIZED") {
+      const userStore = useUserStore();
+      const url = userStore.userType === "student" ? "/student/survey/" : `/guidance/survey/${this.currentAnsweredSurvey.email}`;
+      
+      try {
+        await fetch(import.meta.env.VITE_URL + url, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${userStore.access_token}`,
+          },
+          body: JSON.stringify({
+            grade: this.currentSurvey.grade,
+            status: status,
+            answers: JSON.stringify(this.currentResponse),
+          })
+        })
+      } catch (error) {
+        console.error("Error posting survey:", error);
+      }
+    },
+    async saveSurvey() {
+      const userStore = useUserStore();
       this.loading = true;
       this.checkSurveyAnswers();
+
+      if (this.missingAnswers.length !== 0) return;
+
+      if (userStore.userType === "student") {
+        await this.postSurvey("COMPLETE");
+      } else if (userStore.userType === "guidance") {
+        await this.postSurvey("FINALIZED");
+      }
+      this.loading = false;
     },
     
   },
