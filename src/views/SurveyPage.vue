f3a61636c53a2c8a262666ddce6a590455b63348
DariaG16 <dariagolitsyna16@gmail.com>
2024-03-14 10:58:33 -0400

    saving doesnt update saved survey correctly

--- src/views/SurveyPage.vue
+++ src/views/SurveyPage.vue
@@ -1,32 +1,57 @@
 <template>
   <div class="h-[80vh] flex flex-col justify-center items-center space-y-8">
     <p v-if="surveyStore.loading">Setting things up...</p>
-    <div v-else class="w-11/12 md:w-4/5 lg:w-3/4 flex flex-col items-center min-h-[20rem] h-5/6 overflow mt-6">
+    <div
+      v-else
+      class="w-11/12 md:w-4/5 lg:w-3/4 flex flex-col items-center min-h-[20rem] h-5/6 overflow mt-6"
+    >
       <div class="mt-5">
         <h1 class="text-4xl font-semibold mb-6">
           {{ surveyStore.currentSurvey.grade }} Year Survey
         </h1>
       </div>
       <div class="h-5/6 flex items-center">
-        <generalComponent v-if="currentQuestion.questionType === 'GENERAL'" :question="currentQuestion"
-          :key="currentQuestion.id"></generalComponent>
-        <booleanComponent v-else-if="currentQuestion.questionType === 'BOOLEAN'" :question="currentQuestion"
-          :key="currentQuestion.question"></booleanComponent>
-        <checkboxComponent v-else :question="currentQuestion" :choices="getChoices()" :key="currentQuestion.questionType"
-          :color="'D6EEFF'"></checkboxComponent>
+        <generalComponent
+          v-if="currentQuestion.questionType === 'GENERAL'"
+          :question="currentQuestion"
+          :key="currentQuestion.id"
+        ></generalComponent>
+        <booleanComponent
+          v-else-if="currentQuestion.questionType === 'BOOLEAN'"
+          :question="currentQuestion"
+          :key="currentQuestion.question"
+        ></booleanComponent>
+        <checkboxComponent
+          v-else
+          :question="currentQuestion"
+          :choices="getChoices()"
+          :key="currentQuestion.questionType"
+          :color="'D6EEFF'"
+        ></checkboxComponent>
       </div>
     </div>
-    <div class="h-1/6 w-11/12 md:w-4/5 lg:w-3/4 flex justify-between items-start px-4">
-      <button @click="previousQuestion()" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:bg-stone-400"
-        :disabled="min">
+    <div
+      class="h-1/6 w-11/12 md:w-4/5 lg:w-3/4 flex justify-between items-start px-4"
+    >
+      <button
+        @click="previousQuestion()"
+        class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:bg-stone-400"
+        :disabled="min"
+      >
         Back
       </button>
-      <button @click="nextQuestion()" class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:hidden"
-        :disabled="max">
+      <button
+        @click="nextQuestion()"
+        class="bg-[#6A9FD1] text-white w-24 h-10 rounded-md disabled:hidden"
+        :disabled="max"
+      >
         Next
       </button>
       <RouterLink to="/student/survey/review" v-if="max">
-        <button class="bg-emerald-600 text-white w-auto px-3 h-10 rounded-md inline disabled:hidden" :disabled="!max">
+        <button
+          class="bg-emerald-600 text-white w-auto px-3 h-10 rounded-md inline disabled:hidden"
+          :disabled="!max"
+        >
           Review and Submit
         </button>
       </RouterLink>
@@ -48,7 +73,7 @@ import { useSurveyStore } from "../stores/survey";
 import { surveyQuestion, course, surveyAnswer } from "../types/interface";
 import { onBeforeRouteLeave } from "vue-router";
 
-document.title = 'Survey | SITHS Course Selection'
+document.title = "Survey | SITHS Course Selection";
 
 const userStore = useUserStore();
 const surveyStore = useSurveyStore();
@@ -64,7 +89,7 @@ async function startSurvey() {
   await surveyStore.fetchSurvey();
   await surveyStore.saveSurvey();
 }
-startSurvey()
+startSurvey();
 
 const previousQuestion = () => {
   currentIndex.value--;
@@ -93,38 +118,52 @@ const getChoices = () => {
 };
 
 onBeforeRouteLeave((to, from, next) => {
-  if (JSON.stringify(surveyStore.currentResponse) === surveyStore.currentAnsweredSurvey.answers || to.path === '/student/survey/review') {
-    window.removeEventListener('beforeunload', reminder)
-    next()
+  if (
+    JSON.stringify(surveyStore.currentResponse) ===
+      surveyStore.currentAnsweredSurvey.answers ||
+    to.path === "/student/survey/review"
+  ) {
+    window.removeEventListener("beforeunload", reminder);
+    next();
   } else {
-    const answer = window.confirm('Changes you made might not be saved.')
+    const answer = window.confirm("Changes you made might not be saved.");
     if (answer) {
-      window.removeEventListener('beforeunload', reminder)
-      next()
+      window.removeEventListener("beforeunload", reminder);
+      next();
     } else {
-      next(false)
+      next(false);
     }
   }
-})
+});
 
-const reminder = (e: { preventDefault: () => void; returnValue: string; }) => {
+const reminder = (e: { preventDefault: () => void; returnValue: string }) => {
   e.preventDefault();
-  e.returnValue = '';
+  e.returnValue = "";
 };
 
-watch(() => surveyStore.currentResponse, (newResponse, oldResponse) => {
-  if (JSON.stringify(newResponse) === surveyStore.currentAnsweredSurvey.answers) {
-    window.removeEventListener('beforeunload', reminder)
-  } else {
-    window.addEventListener('beforeunload', reminder);
-  }
-}, { deep: true })
+watch(
+  () => surveyStore.currentResponse,
+  (newResponse, oldResponse) => {
+    if (
+      JSON.stringify(newResponse) === surveyStore.currentAnsweredSurvey.answers
+    ) {
+      window.removeEventListener("beforeunload", reminder);
+    } else {
+      window.addEventListener("beforeunload", reminder);
+    }
+  },
+  { deep: true }
+);
 
-watch(() => surveyStore.currentAnsweredSurvey, (newResponse, oldResponse) => {
-  if (newResponse.answers === JSON.stringify(surveyStore.currentResponse)) {
-    window.removeEventListener('beforeunload', reminder)
-  } else {
-    window.addEventListener('beforeunload', reminder);
-  }
-}, { deep: true })
+watch(
+  () => surveyStore.currentAnsweredSurvey,
+  (newResponse, oldResponse) => {
+    if (newResponse.answers === JSON.stringify(surveyStore.currentResponse)) {
+      window.removeEventListener("beforeunload", reminder);
+    } else {
+      window.addEventListener("beforeunload", reminder);
+    }
+  },
+  { deep: true }
+);
 </script>
