60b08e7a059143e2a35a3545c6ddc1ba2a0cf768
bridgetf7 <fbridget0404@gmail.com>
2025-02-28 10:42:10 -0500

    404 page

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full h-[80vh] flex flex-col justify-center items-center">
+        <h1 class="text-6xl font-bold">404</h1>
+        <p class="text-xl my-2">The route you have requested does not exist.</p>
+        <routerLink to="/" class="bg-primary-s w-40 h-12 rounded-md text-xl font-semibold hover:bg-other-s justify-center items-center flex">
+            Back to home
+        </routerLink>   
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped></style>
