c06c484c453a961d0a4724c5cbb8c557e2fc56f1
bridgetf7 <fbridget0404@gmail.com>
2025-03-01 14:31:24 -0500

    Update ResetPassword.vue

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,42 +1,30 @@
 <template>
   <div class="min-h-screen flex flex-col items-center">
     <div
-      class="mt-10 max-w-lg w-full bg-white sm:shadow-lg rounded-2xl p-6 sm:p-8 md:p-10 lg:p-16 sm:border-2 sm:border-primary-s text-center"
-    >
+      class="mt-10 max-w-lg w-full sm:shadow-lg rounded-2xl p-6 sm:p-8 md:p-10 lg:p-16 sm:border-2 sm:border-primary-s text-center">
       <h2 class="text-2xl font-bold text-secondary-s">Reset Your Password</h2>
-      <p class="mt-4 text-primary-s">
+      <p class="mt-4 text-primary-s bg-none">
         Enter your email to receive a reset link.
       </p>
-      <form class="mt-6" @submit.prevent="userStore.resetPassword(email)">
-        <div class="mb-6">
-          <Input
-            type="email"
-            label="email"
-            required
-            :value="email"
-            @update="(newValue) => (email = newValue)"
-            >Enter your email...</Input
-          >
-        </div>
-        <button
-          type="submit"
-          class="w-full bg-primary-s text-white py-4 sm:py-5 md:py-6 rounded-xl hover:bg-secondary-s transition"
-        >
+      <div class="mt-6" @submit.prevent="userStore.resetPassword(email)">
+        <form class="mb-6 flex flex-col">
+          <Input type="email" label="email" required :value="email" @update="(newValue) => (email = newValue)" class="h-full">Email Address</Input>
+        </form>
+        <button type="submit"
+          class="w-full bg-primary-s text-white py-2 sm:py-3 md:py-4 rounded-lg hover:bg-secondary-s transition duration-300">
           Send Reset Link
         </button>
-      </form>
+      </div>
       <p class="mt-6 text-primary-s">
         Remembered your password?
-        <router-link
-          to="/login"
-          class="text-primary-s hover:text-secondary-s transition"
-          >Login here</router-link
-        >
+        <RouterLink to="/login" class="text-primary-s hover:text-secondary-s transition duration-300 underline">Login
+          here</RouterLink>
       </p>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
 import Input from "../components/Basic/Input.vue";
 import { useUserStore } from "../stores/user";
 import { ref } from "vue";
