1ef400f92d5d55180bcb71a0fb5a3e2939949b4e
Samuel Kipnis <sam.kipnis@gmail.com>
2025-04-01 11:01:47 -0400

    r

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Student, Meeting, GuidanceStudent, Stats } from "../types/interface";
 import { useSurveyStore } from "./survey";
 import { useRouter } from "vue-router";
@@ -21,20 +22,46 @@ export const useUserStore = defineStore("user", () => {
   const viewedStudents = ref<GuidanceStudent[]>([]);
   const meetings = ref<Meeting[]>([]);
   const meetingsFetched = ref(false);
+=======
+import { Student, Meeting, GuidanceStudent, Stats } from '../types/interface';
+import { useSurveyStore } from './survey';
+import { useRouter } from 'vue-router';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-  async function fetchData(url: string, method?: string, body?: any) {
-    loading.value = true;
-    const options: RequestInit = { credentials: "include" };
-    if (method) {
-      options["method"] = method;
-      options["headers"] = { "Content-Type": "application/json" };
-      options["body"] = JSON.stringify(body);
-    }
-    const res = await fetch(import.meta.env.VITE_URL + url, options);
-    loading.value = false;
-    return res;
-  }
+export const useUserStore = defineStore('user', () => {
+	const router = useRouter();
+	const surveyStore = useSurveyStore();
+	const loading = ref(false);
+	const profileID = ref(0);
+	const initComplete = ref(false);
+	const isAuth = ref(false);
+	const firstName = ref('');
+	const lastName = ref('');
+	const email = ref('');
+	const isGuidance = ref(false);
+	const student = ref<Student>({} as Student);
+	const studentList = ref<GuidanceStudent[]>([]);
+	const viewedStudents = ref<GuidanceStudent[]>([]);
+	const meetings = ref<Meeting[]>([]);
+	const meetingsFetched = ref(false);
+>>>>>>> Stashed changes
 
+	async function fetchData(url: string, method?: string, body?: any) {
+		loading.value = true;
+		const options: RequestInit = { credentials: 'include' };
+		if (method) {
+			options['method'] = method;
+			options['headers'] = { 'Content-Type': 'application/json' };
+			options['body'] = JSON.stringify(body);
+		}
+		console.log(options, method);
+		const res = await fetch(import.meta.env.VITE_URL + url, options);
+		loading.value = false;
+		return res;
+	}
+
+<<<<<<< Updated upstream
   function setPopup(message: string, error: boolean = false) {
     popup.message = "";
     popup.update = !popup.update;
@@ -131,35 +158,77 @@ export const useUserStore = defineStore("user", () => {
     $reset();
     router.push("/");
   }
+=======
+	async function init() {
+		const res = await fetchData('init/');
+		initComplete.value = true;
+		if (!res.ok || res.status === 204) return;
+		const data = await res.json();
+		profileID.value = data.id;
+		firstName.value = data.firstName[0] + data.firstName.slice(1).toLowerCase();
+		lastName.value = data.lastName[0] + data.lastName.slice(1).toLowerCase();
+		email.value = data.email;
+		isGuidance.value = data.isGuidance;
+		if (!isGuidance.value) {
+			student.value = data.student;
+			if (data.student.status === 'Finalized') surveyStore.open = false;
+		} else await getStudents();
+		isAuth.value = true;
+	}
+	async function login(username: string, password: string) {
+		const res = await fetchData('auth/login/', 'POST', {
+			username: username.toLowerCase(),
+			password: password,
+		});
+		if (!res.ok) return await res.json();
+		const data = await res.json();
+		profileID.value = data.id;
+		firstName.value = data.firstName[0] + data.firstName.slice(1).toLowerCase();
+		lastName.value = data.lastName[0] + data.lastName.slice(1).toLowerCase();
+		email.value = data.email;
+		isGuidance.value = data.isGuidance;
+		if (!isGuidance.value) {
+			student.value = data.student;
+			if (data.student.status === 'Finalized') surveyStore.open = false;
+		} else await getStudents();
+		isAuth.value = true;
+		router.push(`/${isGuidance.value ? 'guidance' : 'student'}/dashboard`);
+	}
 
-  async function fetchStats() {
-    const res = await fetchData("guidance/stats");
-    const data = await res.json();
-    if (!res.ok) return { years: [], data: [] };
-    return { years: data.map((item: Stats) => item.year), data: data };
-  }
+	async function logout() {
+		const res = await fetchData('auth/logout/', 'POST');
+		if (!res.ok) return await res.json();
+		surveyStore.$reset();
+		$reset();
+		router.push('/');
+	}
+>>>>>>> Stashed changes
 
-  async function getStudents() {
-    const res = await fetchData("guidance/profiles/");
-    if (!res.ok) return await res.json();
-    const data = await res.json();
-    studentList.value = data;
-  }
-  async function changeFlag(
-    student: GuidanceStudent,
-    flag: string,
-    remove: boolean = false
-  ) {
-    const res = await fetchData("guidance/flag/", "POST", {
-      id: student.id,
-      remove,
-    });
-    if (!res.ok) return await res.json();
-    const data = await res.json();
-    // const index = studentList.value.findIndex((student) => student.id === id);
-    // studentList.value[index] = data.flag;
-  }
+	async function fetchStats() {
+		const res = await fetchData('guidance/stats', 'GET');
+		const data = await res.json();
+		if (!res.ok) return { years: [], data: [] };
+		return { years: data.map((item: Stats) => item.year), data: data };
+	}
 
+	async function getStudents() {
+		const res = await fetchData('guidance/profiles/');
+		if (!res.ok) return await res.json();
+		const data = await res.json();
+		studentList.value = data;
+	}
+	async function changeFlag(student: GuidanceStudent, flag: string, remove: boolean = false) {
+		const res = await fetchData('guidance/flag/', 'POST', {
+			id: student.id,
+			remove,
+		});
+		if (!res.ok) return await res.json();
+		const data = await res.json();
+		// const index = studentList.value.findIndex((student) => student.id === id);
+		// studentList.value[index] = data.flag;
+	}
+
+<<<<<<< Updated upstream
   async function getMeetings() {
     const res = await fetchData("guidance/meetings/");
     if (!res.ok) return await res.json();
@@ -245,4 +314,64 @@ export const useUserStore = defineStore("user", () => {
     resetPasswordConfirm,
     $reset,
   };
+=======
+	async function getMeetings() {
+		const res = await fetchData('guidance/meetings/');
+		if (!res.ok) return await res.json();
+		const data = await res.json();
+		meetings.value = data.map((meeting: Meeting) => ({
+			...meeting,
+			meetingDate: new Date(meeting.meetingDate),
+			name: meeting.name
+				.split(',')
+				.map((s) => s[0].toUpperCase() + s.slice(1).toLowerCase())
+				.join(', '),
+		}));
+		meetingsFetched.value = true;
+	}
+
+	async function changeMeeting(id: number, deleteMeeting: boolean, meetingISO?: string, description?: string, notify?: boolean) {
+		if (deleteMeeting) return;
+		const res = await fetchData('guidance/meeting/', 'POST', {
+			meetingISO,
+			description,
+		});
+		if (!res.ok) return await res.json();
+		const data = await res.json();
+		// guidanceMeetings.value.push(data);
+	}
+
+	function $reset() {
+		profileID.value = 0;
+		initComplete.value = false;
+		isAuth.value = false;
+		firstName.value = '';
+		lastName.value = '';
+		email.value = '';
+		isGuidance.value = false;
+		student.value = {} as Student;
+	}
+
+	return {
+		init,
+		login,
+		logout,
+		isAuth,
+		loading,
+		student,
+		lastName,
+		meetings,
+		firstName,
+		isGuidance,
+		fetchStats,
+		changeFlag,
+		getMeetings,
+		studentList,
+		initComplete,
+		changeMeeting,
+		viewedStudents,
+		meetingsFetched,
+		$reset,
+	};
+>>>>>>> Stashed changes
 });
--- src/views/GuidanceDashboard.vue
+++ src/views/GuidanceDashboard.vue
@@ -41,6 +41,7 @@
 <script setup lang="ts">
 import { useUserStore } from "../stores/user";
 const userStore = useUserStore();
+// console.log(await userStore.fetchStats());
 document.title = "Home | SITHS Course Selection";
 
 const name = (str: string) =>
