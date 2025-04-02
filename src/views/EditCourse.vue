2a501b1e1e270e3ef5e17d19af5b64e479dfabbf
Samuel Kipnis <sam.kipnis@gmail.com>
2025-04-02 10:56:25 -0400

    default value, submit button

--- src/views/EditCourse.vue
+++ src/views/EditCourse.vue
@@ -6,41 +6,49 @@
           {{ course.name }}
         </h1>
       </div>
-      <form>
-        <div class="flex flex-col" v-for="key in courseKeys" :key="key">
-          <label :for="course[key]">{{ key }}</label>
+      <form
+        class="m-10 p-5 rounded-xl shadow-md bg-primary-g border-black border-2"
+        @submit.prevent="
+          () => {
+            fetchData('/editcourse/', 'PUT', JSON.stringify(alteredCourse));
+          }
+        "
+      >
+        <div class="flex flex-col p-3" v-for="key in courseKeys" :key="key">
+          <label :for="key" class="text-2xl pb-1 font-bold">{{ key }}</label>
           <input
             type="text"
             v-model="alteredCourse[key]"
             :placeholder="course[key]"
-            :id="course[key]"
+            :id="key"
             class="border-2 border-black rounded-lg p-2 mb-4"
           />
+        </div>
+        <div>
+          <button class="p-5 border-2 border-black bg-white hover:bg-other-g">
+            Submit
+          </button>
         </div>
       </form>
     </div>
   </Suspense>
 </template>
-    
-    <script setup lang="ts">
+   
+<script setup lang="ts">
 import { RouterLink } from "vue-router";
-import { ref, onMounted } from "vue";
-
+import { ref, onMounted, watch } from "vue";
 import { useRoute } from "vue-router";
 
 const alteredCourse = ref<Course>({} as Course);
-
 const route = useRoute();
 const courseId = route.params.id;
 
 function displayGrades(course: Course): string {
   const grades: string[] = [];
-
   if (course.freshman) grades.push("Freshman");
   if (course.sophomore) grades.push("Sophomore");
   if (course.junior) grades.push("Junior");
   if (course.senior) grades.push("Senior");
-
   return grades.join(", ");
 }
 
@@ -49,7 +57,7 @@ async function fetchData(url: string, method?: string, body?: any) {
   if (method) {
     options["method"] = method;
     options["headers"] = { "Content-Type": "application/json" };
-    options["body"] = JSON.stringify(body);
+    options["body"] = body;
   }
   return await fetch(import.meta.env.VITE_URL + url, options);
 }
@@ -70,7 +78,15 @@ let courseKeys = [] as (keyof Course)[];
 onMounted(async () => {
   try {
     course.value = await getCourse();
-    courseKeys = Object.keys(course.value) as (keyof Course)[];
+    // Initialize alteredCourse with a copy of the current course data
+    alteredCourse.value = { ...course.value };
+    courseKeys = Object.keys(course.value).filter(
+      (key) =>
+        // Optionally filter out keys you don't want to edit
+        key !== "id" &&
+        key !== "createdAt" &&
+        typeof course.value[key] !== "object"
+    ) as (keyof Course)[];
   } catch (error) {
     console.error(error);
   }
