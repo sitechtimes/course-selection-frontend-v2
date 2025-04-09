8f60efb08e5e62cee7f50974e535f2fbfbf0672d
RichardM1337 <113366399+RichardM1337@users.noreply.github.com>
2025-04-09 10:21:03 -0400

    removed excess

--- src/views/GuidanceDashboard.vue
+++ src/views/GuidanceDashboard.vue
@@ -41,7 +41,6 @@
 <script setup lang="ts">
 import { useUserStore } from "../stores/user";
 const userStore = useUserStore();
-// console.log(await userStore.fetchStats());
 document.title = "Home | SITHS Course Selection";
 
 const name = (str: string) =>
