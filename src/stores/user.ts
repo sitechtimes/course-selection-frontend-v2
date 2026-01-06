875ba34a4a742122a228ef0e42b20a7ef928a681
Yi Xia <73318855+Shatterdest@users.noreply.github.com>
2026-01-06 10:41:42 -0500

    f

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -53,7 +53,7 @@ export const useUserStore = defineStore("user", () => {
     email.value = data.email;
     isGuidance.value = data.isGuidance;
     if (isGuidance.value) {
-      students.value = data.user.profile;
+      students.value = data.profile;
       await getStudents();
     } else {
       student.value = data.profile;
