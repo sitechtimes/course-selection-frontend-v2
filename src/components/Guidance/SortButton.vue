ac8a64081283707785d00db3ff0a21e68f890d00
RichardM1337 <113366399+RichardM1337@users.noreply.github.com>
2026-02-02 10:44:06 -0500

    Update SortButton.vue
    text width hotfix

--- src/components/Guidance/SortButton.vue
+++ src/components/Guidance/SortButton.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="relative w-44">
-    <div class="h-10 flex items-center justify-between px-4 rounded-lg bg-primary-g text-gray-900 font-semibold cursor-pointer
-             hover:bg-tertiary-g transition" id="sort" @click="isOpen = !isOpen">
+    <div
+      class="h-10 flex w-max items-center justify-between px-4 rounded-lg bg-primary-g text-gray-900 font-semibold cursor-pointer hover:bg-tertiary-g transition"
+      id="sort"
+      @click="isOpen = !isOpen"
+    >
       <p>{{ selected }}</p>
       <span class="text-xl">{{ isOpen ? "⏶" : "⏷" }}</span>
     </div>
 
-    <div v-if="isOpen" class="absolute mt-1 w-full rounded-lg border border-gray-300 bg-white shadow-sm z-10">
-      <button v-for="x in menuArray" :key="x.sortBy" @click="filter(x.sortBy, x.text); isOpen = false"
-        class="w-full text-left px-4 py-2 hover:bg-gray-100 transition">
+    <div
+      v-if="isOpen"
+      class="absolute mt-1 w-full rounded-lg border border-gray-300 bg-white shadow-sm z-10"
+    >
+      <button
+        v-for="x in menuArray"
+        :key="x.sortBy"
+        @click="
+          filter(x.sortBy, x.text);
+          isOpen = false;
+        "
+        class="w-full text-left px-4 py-2 hover:bg-gray-100 transition"
+      >
         {{ x.text }}
       </button>
     </div>
